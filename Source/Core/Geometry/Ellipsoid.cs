fb516030222eba6684d603fb0a90b395939b9caf
kring789 <kring789@6eae0abb-6483-4171-a68c-a7d7e9ebe9e5>
2010-02-02 04:15:08 +0000

    Add Ellipsoid.Intersections method plus unit tests.

--- Source/Core/Geometry/Ellipsoid.cs
+++ Source/Core/Geometry/Ellipsoid.cs
@@ -57,6 +57,42 @@ namespace MiniGlobe.Core.Geometry
             get { return Math.Max(_radii.X, Math.Max(_radii.Y, _radii.Z)); }
         }
 
+        public double[] Intersections(Vector3d origin, Vector3d direction)
+        {
+            direction.Normalize();
+
+            // By laborious algebraic manipulation....
+            double a = direction.X * direction.X * _oneOverRadiiSquared.X +
+                       direction.Y * direction.Y * _oneOverRadiiSquared.Y +
+                       direction.Z * direction.Z * _oneOverRadiiSquared.Z;
+            double b = 2.0 *
+                       (origin.X * direction.X * _oneOverRadiiSquared.X +
+                        origin.Y * direction.Y * _oneOverRadiiSquared.Y +
+                        origin.Z * direction.Z * _oneOverRadiiSquared.Z);
+            double c = origin.X * origin.X * _oneOverRadiiSquared.X +
+                       origin.Y * origin.Y * _oneOverRadiiSquared.Y +
+                       origin.Z * origin.Z * _oneOverRadiiSquared.Z - 1.0;
+
+            // Solve the quadratic equation: ax^2 + bx + c = 0.
+            // Algorithm is from Wikipedia's "Quadratic equation" topic, and Wikipedia credits
+            // Numerical Recipes in C, section 5.6: "Quadratic and Cubic Equations"
+            double discriminant = b * b - 4 * a * c;
+            if (discriminant < 0.0)
+                return new double[0]; // no intersections
+            else if (discriminant == 0.0)
+                return new double[1] { -0.5 * b / a }; // one intersection at a tangent point
+
+            double t = -0.5 * (b + (b > 0 ? 1.0 : -1.0) * Math.Sqrt(discriminant));
+            double root1 = t / a;
+            double root2 = c / t;
+
+            // Two intersections - return the smallest first.
+            if (root1 < root2)
+                return new double[2] { root1, root2 };
+            else
+                return new double[2] { root2, root1 };
+        }
+
         private readonly Vector3d _radii;
         private readonly Vector3d _oneOverRadiiSquared;
     }
--- Source/Tests/Core/Geometry/EllipsoidTests.cs
+++ Source/Tests/Core/Geometry/EllipsoidTests.cs
@@ -33,5 +33,141 @@ namespace MiniGlobe.Core.Geometry
             Assert.IsTrue(new Vector3d(1, 0, 0).Equals(Ellipsoid.UnitSphere.DeticSurfaceNormal(new Vector3d(1, 0, 0))));
             Assert.IsTrue(new Vector3d(0, 0, 1).Equals(Ellipsoid.UnitSphere.DeticSurfaceNormal(new Vector3d(0, 0, 1))));
         }
+
+        [Test]
+        public void SphereIntersectionsTwoFromOutside()
+        {
+            Ellipsoid unitSphere = Ellipsoid.UnitSphere;
+            
+            double[] intersections = unitSphere.Intersections(new Vector3d(2.0, 0.0, 0.0), new Vector3d(-1.0, 0.0, 0.0));
+            Assert.AreEqual(2, intersections.Length);
+            Assert.AreEqual(1.0, intersections[0], 1e-14);
+            Assert.AreEqual(3.0, intersections[1], 1e-14);
+
+            intersections = unitSphere.Intersections(new Vector3d(0.0, 2.0, 0.0), new Vector3d(0.0, -1.0, 0.0));
+            Assert.AreEqual(2, intersections.Length);
+            Assert.AreEqual(1.0, intersections[0], 1e-14);
+            Assert.AreEqual(3.0, intersections[1], 1e-14);
+
+            intersections = unitSphere.Intersections(new Vector3d(0.0, 0.0, 2.0), new Vector3d(0.0, 0.0, -1.0));
+            Assert.AreEqual(2, intersections.Length);
+            Assert.AreEqual(1.0, intersections[0], 1e-14);
+            Assert.AreEqual(3.0, intersections[1], 1e-14);
+
+            intersections = unitSphere.Intersections(new Vector3d(1.0, 1.0, 0.0), new Vector3d(-1.0, 0.0, 0.0));
+            Assert.AreEqual(1, intersections.Length);
+            Assert.AreEqual(1.0, intersections[0], 1e-14);
+
+            intersections = unitSphere.Intersections(new Vector3d(-2.0, 0.0, 0.0), new Vector3d(1.0, 0.0, 0.0));
+            Assert.AreEqual(2, intersections.Length);
+            Assert.AreEqual(1.0, intersections[0], 1e-14);
+            Assert.AreEqual(3.0, intersections[1], 1e-14);
+
+            intersections = unitSphere.Intersections(new Vector3d(0.0, -2.0, 0.0), new Vector3d(0.0, 1.0, 0.0));
+            Assert.AreEqual(2, intersections.Length);
+            Assert.AreEqual(1.0, intersections[0], 1e-14);
+            Assert.AreEqual(3.0, intersections[1], 1e-14);
+
+            intersections = unitSphere.Intersections(new Vector3d(0.0, 0.0, -2.0), new Vector3d(0.0, 0.0, 1.0));
+            Assert.AreEqual(2, intersections.Length);
+            Assert.AreEqual(1.0, intersections[0], 1e-14);
+            Assert.AreEqual(3.0, intersections[1], 1e-14);
+
+            intersections = unitSphere.Intersections(new Vector3d(-1.0, -1.0, 0.0), new Vector3d(1.0, 0.0, 0.0));
+            Assert.AreEqual(1, intersections.Length);
+            Assert.AreEqual(1.0, intersections[0], 1e-14);
+
+            intersections = unitSphere.Intersections(new Vector3d(-2.0, 0.0, 0.0), new Vector3d(-1.0, 0.0, 0.0));
+            Assert.AreEqual(2, intersections.Length);
+            Assert.AreEqual(-3.0, intersections[0], 1e-14);
+            Assert.AreEqual(-1.0, intersections[1], 1e-14);
+
+            intersections = unitSphere.Intersections(new Vector3d(0.0, -2.0, 0.0), new Vector3d(0.0, -1.0, 0.0));
+            Assert.AreEqual(2, intersections.Length);
+            Assert.AreEqual(-3.0, intersections[0], 1e-14);
+            Assert.AreEqual(-1.0, intersections[1], 1e-14);
+
+            intersections = unitSphere.Intersections(new Vector3d(0.0, 0.0, -2.0), new Vector3d(0.0, 0.0, -1.0));
+            Assert.AreEqual(2, intersections.Length);
+            Assert.AreEqual(-3.0, intersections[0], 1e-14);
+            Assert.AreEqual(-1.0, intersections[1], 1e-14);
+        }
+
+        [Test]
+        public void SphereIntersectionsTwoFromInside()
+        {
+            Ellipsoid unitSphere = Ellipsoid.UnitSphere;
+
+            double[] intersections;
+
+            intersections = unitSphere.Intersections(new Vector3d(0.0, 0.0, 0.0), new Vector3d(0.0, 0.0, 1.0));
+            Assert.AreEqual(2, intersections.Length);
+            Assert.AreEqual(-1.0, intersections[0], 1e-14);
+            Assert.AreEqual(1.0, intersections[1], 1e-14);
+
+            intersections = unitSphere.Intersections(new Vector3d(0.0, 0.5, 0.0), new Vector3d(0.0, 1.0, 0.0));
+            Assert.AreEqual(2, intersections.Length);
+            Assert.AreEqual(-1.5, intersections[0], 1e-14);
+            Assert.AreEqual(0.5, intersections[1], 1e-14);
+
+            intersections = unitSphere.Intersections(new Vector3d(0.0, 0.5, 0.0), new Vector3d(0.0, -1.0, 0.0));
+            Assert.AreEqual(2, intersections.Length);
+            Assert.AreEqual(-0.5, intersections[0], 1e-14);
+            Assert.AreEqual(1.5, intersections[1], 1e-14);
+        }
+
+        [Test]
+        public void SphereIntersectionsFromEdge()
+        {
+            Ellipsoid unitSphere = Ellipsoid.UnitSphere;
+
+            double[] intersections;
+
+            intersections = unitSphere.Intersections(new Vector3d(1.0, 0.0, 0.0), new Vector3d(0.0, 0.0, 1.0));
+            Assert.AreEqual(1, intersections.Length);
+            Assert.AreEqual(0.0, intersections[0], 1e-14);
+
+            intersections = unitSphere.Intersections(new Vector3d(1.0, 0.0, 0.0), new Vector3d(0.0, 1.0, 0.0));
+            Assert.AreEqual(1, intersections.Length);
+            Assert.AreEqual(0.0, intersections[0], 1e-14);
+
+            intersections = unitSphere.Intersections(new Vector3d(1.0, 0.0, 0.0), new Vector3d(1.0, 0.0, 0.0));
+            Assert.AreEqual(2, intersections.Length);
+            Assert.AreEqual(-2.0, intersections[0], 1e-14);
+            Assert.AreEqual(0.0, intersections[1], 1e-14);
+
+            intersections = unitSphere.Intersections(new Vector3d(1.0, 0.0, 0.0), new Vector3d(0.0, 0.0, -1.0));
+            Assert.AreEqual(1, intersections.Length);
+            Assert.AreEqual(0.0, intersections[0], 1e-14);
+
+            intersections = unitSphere.Intersections(new Vector3d(1.0, 0.0, 0.0), new Vector3d(0.0, -1.0, 0.0));
+            Assert.AreEqual(1, intersections.Length);
+            Assert.AreEqual(0.0, intersections[0], 1e-14);
+
+            intersections = unitSphere.Intersections(new Vector3d(1.0, 0.0, 0.0), new Vector3d(-1.0, 0.0, 0.0));
+            Assert.AreEqual(2, intersections.Length);
+            Assert.AreEqual(0.0, intersections[0], 1e-14);
+            Assert.AreEqual(2.0, intersections[1], 1e-14);
+        }
+
+        [Test]
+        public void SphereIntersectionsNoIntersection()
+        {
+            Ellipsoid unitSphere = Ellipsoid.UnitSphere;
+
+            double[] intersections;
+
+            intersections = unitSphere.Intersections(new Vector3d(2.0, 0.0, 0.0), new Vector3d(0.0, 0.0, 1.0));
+            Assert.AreEqual(0, intersections.Length);
+
+            intersections = unitSphere.Intersections(new Vector3d(2.0, 0.0, 0.0), new Vector3d(0.0, 0.0, -1.0));
+            Assert.AreEqual(0, intersections.Length);
+
+            intersections = unitSphere.Intersections(new Vector3d(2.0, 0.0, 0.0), new Vector3d(0.0, 1.0, 0.0));
+            Assert.AreEqual(0, intersections.Length);
+
+            intersections = unitSphere.Intersections(new Vector3d(2.0, 0.0, 0.0), new Vector3d(0.0, -1.0, 0.0));
+            Assert.AreEqual(0, intersections.Length);
+        }
     }
 }
