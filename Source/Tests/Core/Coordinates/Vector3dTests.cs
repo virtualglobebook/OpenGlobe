ab6df3a98d4ed878d0e718df16234e45b17672b5
kring789 <kring789@6eae0abb-6483-4171-a68c-a7d7e9ebe9e5>
2010-02-18 03:16:51 +0000

    Added a really basic Vector3d type and tests.

---
+++ Source/Tests/Core/Coordinates/Vector3dTests.cs
@@ -0,0 +1,141 @@
+﻿#region License
+//
+// (C) Copyright 2010 Patrick Cozzi, Deron Ohlarik, and Kevin Ring
+//
+// Distributed under the Boost Software License, Version 1.0.
+// See License.txt or http://www.boost.org/LICENSE_1_0.txt.
+//
+#endregion
+
+using NUnit.Framework;
+
+namespace MiniGlobe.Core.Coordinates
+{
+    [TestFixture]
+    public class Vector3dTests
+    {
+        [Test]
+        public void Construct()
+        {
+            Vector3d v = new Vector3d(1.0, 2.0, 3.0);
+            Assert.AreEqual(1.0, v.X);
+            Assert.AreEqual(2.0, v.Y);
+            Assert.AreEqual(3.0, v.Z);
+        }
+
+        [Test]
+        public void Magnitude()
+        {
+            Vector3d v = new Vector3d(3.0, 4.0, 0.0);
+            Assert.AreEqual(25.0, v.MagnitudeSquared, 1e-14);
+            Assert.AreEqual(5.0, v.Magnitude, 1e-14);
+
+            v = new Vector3d(3.0, 0.0, 4.0);
+            Assert.AreEqual(25.0, v.MagnitudeSquared, 1e-14);
+            Assert.AreEqual(5.0, v.Magnitude, 1e-14);
+
+            v = new Vector3d(0.0, 3.0, 4.0);
+            Assert.AreEqual(25.0, v.MagnitudeSquared, 1e-14);
+            Assert.AreEqual(5.0, v.Magnitude, 1e-14);
+        }
+
+        [Test]
+        public void Normalize()
+        {
+            Vector3d v, n1, n2;
+            double magnitude;
+
+            v = new Vector3d(3.0, 4.0, 0.0);
+            n1 = v.Normalize();
+            n2 = v.Normalize(out magnitude);
+            Assert.AreEqual(1.0, n1.Magnitude, 1e-14);
+            Assert.AreEqual(1.0, n2.Magnitude, 1e-14);
+            Assert.AreEqual(5.0, magnitude, 1e-14);
+
+            v = new Vector3d(3.0, 0.0, 4.0);
+            n1 = v.Normalize();
+            n2 = v.Normalize(out magnitude);
+            Assert.AreEqual(1.0, n1.Magnitude, 1e-14);
+            Assert.AreEqual(1.0, n2.Magnitude, 1e-14);
+            Assert.AreEqual(5.0, magnitude, 1e-14);
+
+            v = new Vector3d(0.0, 3.0, 4.0);
+            n1 = v.Normalize();
+            n2 = v.Normalize(out magnitude);
+            Assert.AreEqual(1.0, n1.Magnitude, 1e-14);
+            Assert.AreEqual(1.0, n2.Magnitude, 1e-14);
+            Assert.AreEqual(5.0, magnitude, 1e-14);
+        }
+
+        [Test]
+        public void Add()
+        {
+            Vector3d v1 = new Vector3d(1.0, 2.0, 3.0);
+            Vector3d v2 = new Vector3d(4.0, 5.0, 6.0);
+            
+            Vector3d v3 = v1 + v2;
+            Assert.AreEqual(5.0, v3.X, 1e-14);
+            Assert.AreEqual(7.0, v3.Y, 1e-14);
+            Assert.AreEqual(9.0, v3.Z, 1e-14);
+
+            Vector3d v4 = v1.Add(v2);
+            Assert.AreEqual(5.0, v4.X, 1e-14);
+            Assert.AreEqual(7.0, v4.Y, 1e-14);
+            Assert.AreEqual(9.0, v4.Z, 1e-14);
+        }
+
+        [Test]
+        public void Subtract()
+        {
+            Vector3d v1 = new Vector3d(1.0, 2.0, 3.0);
+            Vector3d v2 = new Vector3d(4.0, 5.0, 6.0);
+
+            Vector3d v3 = v1 - v2;
+            Assert.AreEqual(-3.0, v3.X, 1e-14);
+            Assert.AreEqual(-3.0, v3.Y, 1e-14);
+            Assert.AreEqual(-3.0, v3.Z, 1e-14);
+
+            Vector3d v4 = v1.Subtract(v2);
+            Assert.AreEqual(-3.0, v4.X, 1e-14);
+            Assert.AreEqual(-3.0, v4.Y, 1e-14);
+            Assert.AreEqual(-3.0, v4.Z, 1e-14);
+        }
+
+        [Test]
+        public void Multiply()
+        {
+            Vector3d v1 = new Vector3d(1.0, 2.0, 3.0);
+
+            Vector3d v2 = v1 * 5.0;
+            Assert.AreEqual(5.0, v2.X, 1e-14);
+            Assert.AreEqual(10.0, v2.Y, 1e-14);
+            Assert.AreEqual(15.0, v2.Z, 1e-14);
+
+            Vector3d v3 = 5.0 * v1;
+            Assert.AreEqual(5.0, v3.X, 1e-14);
+            Assert.AreEqual(10.0, v3.Y, 1e-14);
+            Assert.AreEqual(15.0, v3.Z, 1e-14);
+
+            Vector3d v4 = v1.Multiply(5.0);
+            Assert.AreEqual(5.0, v4.X, 1e-14);
+            Assert.AreEqual(10.0, v4.Y, 1e-14);
+            Assert.AreEqual(15.0, v4.Z, 1e-14);
+        }
+
+        [Test]
+        public void Divide()
+        {
+            Vector3d v1 = new Vector3d(10.0, 20.0, 30.0);
+
+            Vector3d v2 = v1 / 5.0;
+            Assert.AreEqual(2.0, v2.X, 1e-14);
+            Assert.AreEqual(4.0, v2.Y, 1e-14);
+            Assert.AreEqual(6.0, v2.Z, 1e-14);
+
+            Vector3d v3 = v1.Divide(5.0);
+            Assert.AreEqual(2.0, v3.X, 1e-14);
+            Assert.AreEqual(4.0, v3.Y, 1e-14);
+            Assert.AreEqual(6.0, v3.Z, 1e-14);
+        }
+    }
+}
