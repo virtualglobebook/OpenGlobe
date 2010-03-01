f9cfae178c9a1fd7395b1ed82632a1d334600427
kring789 <kring789@6eae0abb-6483-4171-a68c-a7d7e9ebe9e5>
2010-03-01 03:49:55 +0000

    Very basic Cartographic3D type, and tests.

---
+++ Source/Core/Cartographic3D.cs
@@ -0,0 +1,71 @@
+﻿#region License
+//
+// (C) Copyright 2010 Patrick Cozzi, Deron Ohlarik, and Kevin Ring
+//
+// Distributed under the Boost Software License, Version 1.0.
+// See License.txt or http://www.boost.org/LICENSE_1_0.txt.
+//
+#endregion
+
+using System;
+
+namespace MiniGlobe.Core
+{
+    public struct Cartographic3D : IEquatable<Cartographic3D>
+    {
+        public Cartographic3D(double longitude, double latitude, double height)
+        {
+            _longitude = longitude;
+            _latitude = latitude;
+            _height = height;
+        }
+
+        public double Longitude
+        {
+            get { return _longitude; }
+        }
+
+        public double Latitude
+        {
+            get { return _latitude; }
+        }
+
+        public double Height
+        {
+            get { return _height; }
+        }
+
+        public bool Equals(Cartographic3D other)
+        {
+            return _longitude == other._longitude && _latitude == other._latitude && _height == other._height;
+        }
+
+        public static bool operator ==(Cartographic3D left, Cartographic3D right)
+        {
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(Cartographic3D left, Cartographic3D right)
+        {
+            return !left.Equals(right);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is Cartographic3D)
+            {
+                return Equals((Cartographic3D)obj);
+            }
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return _longitude.GetHashCode() ^ _latitude.GetHashCode() ^ _height.GetHashCode();
+        }
+
+        private double _longitude;
+        private double _latitude;
+        private double _height;
+    }
+}
---
+++ Source/Tests/Core/Cartographic3DTests.cs
@@ -0,0 +1,95 @@
+﻿#region License
+//
+// (C) Copyright 2010 Patrick Cozzi, Deron Ohlarik, and Kevin Ring
+//
+// Distributed under the Boost Software License, Version 1.0.
+// See License.txt or http://www.boost.org/LICENSE_1_0.txt.
+//
+#endregion
+
+using NUnit.Framework;
+
+namespace MiniGlobe.Core
+{
+    [TestFixture]
+    public class Cartographic3DTests
+    {
+        [Test]
+        public void Construct()
+        {
+            Cartographic3D a = new Cartographic3D(1.0, 2.0, 3.0);
+            Assert.AreEqual(1.0, a.Longitude);
+            Assert.AreEqual(2.0, a.Latitude);
+            Assert.AreEqual(3.0, a.Height);
+        }
+
+        [Test]
+        public void TestEquals()
+        {
+            Cartographic3D a = new Cartographic3D(1.0, 2.0, 3.0);
+            Cartographic3D b = new Cartographic3D(1.0, 2.0, 4.0);
+            Cartographic3D c = new Cartographic3D(1.0, 3.0, 3.0);
+            Cartographic3D d = new Cartographic3D(2.0, 2.0, 3.0);
+            Cartographic3D e = new Cartographic3D(1.0, 2.0, 3.0);
+
+            object objA = a;
+            object objB = b;
+            object objC = c;
+            object objD = d;
+            object objE = e;
+
+            Assert.IsTrue(a.Equals(a));
+            Assert.IsTrue(a.Equals(e));
+            Assert.IsTrue(e.Equals(a));
+            Assert.IsTrue(a.Equals(objA));
+            Assert.IsTrue(a.Equals(objE));
+            Assert.IsTrue(objA.Equals(objA));
+            Assert.IsTrue(objA.Equals(objE));
+
+            Assert.IsTrue(a == e);
+            Assert.IsTrue(e == a);
+            Assert.IsTrue(a != b);
+            Assert.IsTrue(a != c);
+            Assert.IsTrue(a != d);
+
+            Assert.IsFalse(a.Equals(b));
+            Assert.IsFalse(a.Equals(c));
+            Assert.IsFalse(a.Equals(d));
+
+            Assert.IsFalse(b.Equals(a));
+            Assert.IsFalse(c.Equals(a));
+            Assert.IsFalse(d.Equals(a));
+
+            Assert.IsFalse(a.Equals(objB));
+            Assert.IsFalse(a.Equals(objC));
+            Assert.IsFalse(a.Equals(objD));
+
+            Assert.IsFalse(objA.Equals(objB));
+            Assert.IsFalse(objA.Equals(objC));
+            Assert.IsFalse(objA.Equals(objD));
+
+            Assert.IsFalse(a == b);
+            Assert.IsFalse(a == c);
+            Assert.IsFalse(a == d);
+            Assert.IsFalse(a != e);
+
+            Assert.IsFalse(a.Equals(null));
+            Assert.IsFalse(a.Equals(5));
+        }
+
+        [Test]
+        public void TestGetHashCode()
+        {
+            Cartographic3D a = new Cartographic3D(1.0, 2.0, 3.0);
+            Cartographic3D b = new Cartographic3D(1.0, 2.0, 4.0);
+            Cartographic3D c = new Cartographic3D(1.0, 3.0, 3.0);
+            Cartographic3D d = new Cartographic3D(2.0, 2.0, 3.0);
+            Cartographic3D e = new Cartographic3D(1.0, 2.0, 3.0);
+
+            Assert.AreEqual(a.GetHashCode(), e.GetHashCode());
+            Assert.AreNotEqual(a.GetHashCode(), b.GetHashCode());
+            Assert.AreNotEqual(a.GetHashCode(), c.GetHashCode());
+            Assert.AreNotEqual(a.GetHashCode(), d.GetHashCode());
+        }
+    }
+}
