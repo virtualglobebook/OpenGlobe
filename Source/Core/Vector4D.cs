7fdb92964d399422837ed7e44615753feefbeca6
kring789 <kring789@6eae0abb-6483-4171-a68c-a7d7e9ebe9e5>
2010-03-11 03:14:45 +0000

    Add Vector4D and tests.

--- Source/Core/Vector4D.cs
+++ Source/Core/Vector4D.cs
@@ -16,46 +16,52 @@ using System.Globalization;
 namespace MiniGlobe.Core
 {
     /// <summary>
-    /// A set of 3-dimensional cartesian coordinates where the three components,
-    /// <see cref="X"/>, <see cref="Y"/>, and <see cref="Z"/>, are represented as
-    /// double-precision (64-bit) floating point numbers.
+    /// A set of 4-dimensional cartesian coordinates where the four components,
+    /// <see cref="X"/>, <see cref="Y"/>, <see cref="Z"/>, and <see cref="W"/>
+    /// are represented as double-precision (64-bit) floating point numbers.
     /// </summary>
     [Serializable]
     [StructLayout(LayoutKind.Sequential)]
-    public struct Vector3D : IEquatable<Vector3D>
+    public struct Vector4D : IEquatable<Vector4D>
     {
-        public static readonly int SizeInBytes = Marshal.SizeOf(typeof(Vector3D));
+        public static readonly int SizeInBytes = Marshal.SizeOf(typeof(Vector4D));
 
-        public static Vector3D Zero
+        public static Vector4D Zero
         {
-            get { return new Vector3D(0.0, 0.0, 0.0); }
+            get { return new Vector4D(0.0, 0.0, 0.0, 0.0); }
         }
 
-        public static Vector3D UnitX
+        public static Vector4D UnitX
         {
-            get { return new Vector3D(1.0, 0.0, 0.0); }
+            get { return new Vector4D(1.0, 0.0, 0.0, 0.0); }
         }
 
-        public static Vector3D UnitY
+        public static Vector4D UnitY
         {
-            get { return new Vector3D(0.0, 1.0, 0.0); }
+            get { return new Vector4D(0.0, 1.0, 0.0, 0.0); }
         }
 
-        public static Vector3D UnitZ
+        public static Vector4D UnitZ
         {
-            get { return new Vector3D(0.0, 0.0, 1.0); }
+            get { return new Vector4D(0.0, 0.0, 1.0, 0.0); }
         }
 
-        public static Vector3D Undefined
+        public static Vector4D UnitW
         {
-            get { return new Vector3D(Double.NaN, Double.NaN, Double.NaN); }
+            get { return new Vector4D(0.0, 0.0, 0.0, 1.0); }
         }
 
-        public Vector3D(double x, double y, double z)
+        public static Vector4D Undefined
+        {
+            get { return new Vector4D(Double.NaN, Double.NaN, Double.NaN, Double.NaN); }
+        }
+
+        public Vector4D(double x, double y, double z, double w)
         {
             _x = x;
             _y = y;
             _z = z;
+            _w = w;
         }
 
         public double X
@@ -73,14 +79,24 @@ namespace MiniGlobe.Core
             get { return _z; }
         }
 
+        public double W
+        {
+            get { return _w; }
+        }
+
         public Vector2D XY
         {
             get { return new Vector2D(X, Y); }
         }
 
+        public Vector3D XYZ
+        {
+            get { return new Vector3D(X, Y, Z); }
+        }
+
         public double MagnitudeSquared
         {
-            get { return _x * _x + _y * _y + _z * _z; }
+            get { return _x * _x + _y * _y + _z * _z + _w * _w; }
         }
 
         public double Magnitude
@@ -93,152 +109,151 @@ namespace MiniGlobe.Core
             get { return Double.IsNaN(_x); }
         }
 
-        public Vector3D Normalize(out double magnitude)
+        public Vector4D Normalize(out double magnitude)
         {
             magnitude = Magnitude;
             return this / magnitude;
         }
 
-        public Vector3D Normalize()
+        public Vector4D Normalize()
         {
             double magnitude;
             return Normalize(out magnitude);
         }
 
-        public Vector3D Cross(Vector3D other)
+        public double Dot(Vector4D other)
         {
-            return new Vector3D(Y * other.Z - Z * other.Y,
-                                Z * other.X - X * other.Z,
-                                X * other.Y - Y * other.X);
+            return X * other.X + Y * other.Y + Z * other.Z + W * other.W;
         }
 
-        public double Dot(Vector3D other)
-        {
-            return X * other.X + Y * other.Y + Z * other.Z;
-        }
-
-        public Vector3D Add(Vector3D addend)
+        public Vector4D Add(Vector4D addend)
         {
             return this + addend;
         }
 
-        public Vector3D Subtract(Vector3D subtrahend)
+        public Vector4D Subtract(Vector4D subtrahend)
         {
             return this - subtrahend;
         }
 
-        public Vector3D Multiply(double scalar)
+        public Vector4D Multiply(double scalar)
         {
             return this * scalar;
         }
 
-        public Vector3D MultiplyComponents(Vector3D scale)
+        public Vector4D MultiplyComponents(Vector4D scale)
         {
-            return new Vector3D(X * scale.X, Y * scale.Y, Z * scale.Z);
+            return new Vector4D(X * scale.X, Y * scale.Y, Z * scale.Z, W * scale.W);
         }
 
-        public Vector3D Divide(double scalar)
+        public Vector4D Divide(double scalar)
         {
             return this / scalar;
         }
 
-        public Vector3D MostOrthogonalAxis
+        public Vector4D MostOrthogonalAxis
         {
             get
             {
                 double x = Math.Abs(X);
                 double y = Math.Abs(Y);
                 double z = Math.Abs(Z);
+                double w = Math.Abs(W);
 
-                if ((x < y) && (x < z))
+                if ((x < y) && (x < z) && (x < w))
                 {
                     return UnitX;
                 }
-                else if ((y < x) && (y < z))
+                else if ((y < x) && (y < z) && (y < w))
                 {
                     return UnitY;
                 }
-                else
+                else if ((z < x) && (z < y) && (z < w))
                 {
                     return UnitZ;
+                }
+                else
+                {
+                    return UnitW;
                 }
             }
         }
 
-        public bool EqualsEpsilon(Vector3D other, double epsilon)
+        public bool EqualsEpsilon(Vector4D other, double epsilon)
         {
-            return
-                (Math.Abs(_x - other._x) <= epsilon) &&
-                (Math.Abs(_y - other._y) <= epsilon) &&
-                (Math.Abs(_z - other._z) <= epsilon);
+            return (Math.Abs(_x - other._x) <= epsilon) &&
+                   (Math.Abs(_y - other._y) <= epsilon) &&
+                   (Math.Abs(_z - other._z) <= epsilon) &&
+                   (Math.Abs(_w - other._w) <= epsilon);
         }
 
-        public bool Equals(Vector3D other)
+        public bool Equals(Vector4D other)
         {
-            return _x == other._x && _y == other._y && _z == other._z;
+            return _x == other._x && _y == other._y && _z == other._z && _w == other._w;
         }
 
-        public static Vector3D operator -(Vector3D vector)
+        public static Vector4D operator -(Vector4D vector)
         {
-            return new Vector3D(-vector.X, -vector.Y, -vector.Z);
+            return new Vector4D(-vector.X, -vector.Y, -vector.Z, -vector.W);
         }
 
-        public static Vector3D operator +(Vector3D left, Vector3D right)
+        public static Vector4D operator +(Vector4D left, Vector4D right)
         {
-            return new Vector3D(left._x + right._x, left._y + right._y, left._z + right._z);
+            return new Vector4D(left._x + right._x, left._y + right._y, left._z + right._z, left._w + right._w);
         }
 
-        public static Vector3D operator -(Vector3D left, Vector3D right)
+        public static Vector4D operator -(Vector4D left, Vector4D right)
         {
-            return new Vector3D(left._x - right._x, left._y - right._y, left._z - right._z);
+            return new Vector4D(left._x - right._x, left._y - right._y, left._z - right._z, left._w - right._w);
         }
 
-        public static Vector3D operator *(Vector3D left, double right)
+        public static Vector4D operator *(Vector4D left, double right)
         {
-            return new Vector3D(left._x * right, left._y * right, left._z * right);
+            return new Vector4D(left._x * right, left._y * right, left._z * right, left._w * right);
         }
 
-        public static Vector3D operator *(double left, Vector3D right)
+        public static Vector4D operator *(double left, Vector4D right)
         {
             return right * left;
         }
 
-        public static Vector3D operator /(Vector3D left, double right)
+        public static Vector4D operator /(Vector4D left, double right)
         {
-            return new Vector3D(left._x / right, left._y / right, left._z / right);
+            return new Vector4D(left._x / right, left._y / right, left._z / right, left._w / right);
         }
 
-        public static bool operator ==(Vector3D left, Vector3D right)
+        public static bool operator ==(Vector4D left, Vector4D right)
         {
             return left.Equals(right);
         }
 
-        public static bool operator !=(Vector3D left, Vector3D right)
+        public static bool operator !=(Vector4D left, Vector4D right)
         {
             return !left.Equals(right);
         }
 
         public override bool Equals(object obj)
         {
-            if (obj is Vector3D)
+            if (obj is Vector4D)
             {
-                return Equals((Vector3D)obj);
+                return Equals((Vector4D)obj);
             }
             return false;
         }
 
         public override string ToString()
         {
-            return string.Format(CultureInfo.CurrentCulture, "({0}, {1}, {2})", X, Y, Z);
+            return string.Format(CultureInfo.CurrentCulture, "({0}, {1}, {2}, {3})", X, Y, Z, W);
         }
 
         public override int GetHashCode()
         {
-            return _x.GetHashCode() ^ _y.GetHashCode() ^ _z.GetHashCode();
+            return _x.GetHashCode() ^ _y.GetHashCode() ^ _z.GetHashCode() ^ _w.GetHashCode();
         }
 
         private readonly double _x;
         private readonly double _y;
         private readonly double _z;
+        private readonly double _w;
     }
 }
--- Source/Tests/Core/Vector4DTests.cs
+++ Source/Tests/Core/Vector4DTests.cs
@@ -12,12 +12,12 @@ using NUnit.Framework;
 namespace MiniGlobe.Core
 {
     [TestFixture]
-    public class Vector3DTests
+    public class Vector4DTests
     {
         [Test]
         public void Construct()
         {
-            Vector3D v = new Vector3D(1.0, 2.0, 3.0);
+            Vector4D v = new Vector4D(1.0, 2.0, 3.0, 4.0);
             Assert.AreEqual(1.0, v.X);
             Assert.AreEqual(2.0, v.Y);
             Assert.AreEqual(3.0, v.Z);
@@ -26,15 +26,19 @@ namespace MiniGlobe.Core
         [Test]
         public void Magnitude()
         {
-            Vector3D v = new Vector3D(3.0, 4.0, 0.0);
+            Vector4D v = new Vector4D(3.0, 4.0, 0.0, 0.0);
             Assert.AreEqual(25.0, v.MagnitudeSquared, 1e-14);
             Assert.AreEqual(5.0, v.Magnitude, 1e-14);
 
-            v = new Vector3D(3.0, 0.0, 4.0);
+            v = new Vector4D(3.0, 0.0, 4.0, 0.0);
             Assert.AreEqual(25.0, v.MagnitudeSquared, 1e-14);
             Assert.AreEqual(5.0, v.Magnitude, 1e-14);
 
-            v = new Vector3D(0.0, 3.0, 4.0);
+            v = new Vector4D(0.0, 3.0, 4.0, 0.0);
+            Assert.AreEqual(25.0, v.MagnitudeSquared, 1e-14);
+            Assert.AreEqual(5.0, v.Magnitude, 1e-14);
+
+            v = new Vector4D(0.0, 0.0, 3.0, 4.0);
             Assert.AreEqual(25.0, v.MagnitudeSquared, 1e-14);
             Assert.AreEqual(5.0, v.Magnitude, 1e-14);
         }
@@ -42,24 +46,31 @@ namespace MiniGlobe.Core
         [Test]
         public void Normalize()
         {
-            Vector3D v, n1, n2;
+            Vector4D v, n1, n2;
             double magnitude;
 
-            v = new Vector3D(3.0, 4.0, 0.0);
+            v = new Vector4D(3.0, 4.0, 0.0, 0.0);
             n1 = v.Normalize();
             n2 = v.Normalize(out magnitude);
             Assert.AreEqual(1.0, n1.Magnitude, 1e-14);
             Assert.AreEqual(1.0, n2.Magnitude, 1e-14);
             Assert.AreEqual(5.0, magnitude, 1e-14);
 
-            v = new Vector3D(3.0, 0.0, 4.0);
+            v = new Vector4D(3.0, 0.0, 4.0, 0.0);
             n1 = v.Normalize();
             n2 = v.Normalize(out magnitude);
             Assert.AreEqual(1.0, n1.Magnitude, 1e-14);
             Assert.AreEqual(1.0, n2.Magnitude, 1e-14);
             Assert.AreEqual(5.0, magnitude, 1e-14);
 
-            v = new Vector3D(0.0, 3.0, 4.0);
+            v = new Vector4D(0.0, 3.0, 4.0, 0.0);
+            n1 = v.Normalize();
+            n2 = v.Normalize(out magnitude);
+            Assert.AreEqual(1.0, n1.Magnitude, 1e-14);
+            Assert.AreEqual(1.0, n2.Magnitude, 1e-14);
+            Assert.AreEqual(5.0, magnitude, 1e-14);
+
+            v = new Vector4D(0.0, 0.0, 3.0, 4.0);
             n1 = v.Normalize();
             n2 = v.Normalize(out magnitude);
             Assert.AreEqual(1.0, n1.Magnitude, 1e-14);
@@ -70,16 +81,16 @@ namespace MiniGlobe.Core
         [Test]
         public void NormalizeZeroVector()
         {
-            Vector3D v = new Vector3D(0.0, 0.0, 0.0);
+            Vector4D v = new Vector4D(0.0, 0.0, 0.0, 0.0);
             
-            Vector3D n1 = v.Normalize();
+            Vector4D n1 = v.Normalize();
             Assert.IsNaN(n1.X);
             Assert.IsNaN(n1.Y);
             Assert.IsNaN(n1.Z);
             Assert.IsTrue(n1.IsUndefined);
 
             double magnitude;
-            Vector3D n2 = v.Normalize(out magnitude);
+            Vector4D n2 = v.Normalize(out magnitude);
             Assert.IsNaN(n2.X);
             Assert.IsNaN(n2.Y);
             Assert.IsNaN(n2.Z);
@@ -90,151 +101,178 @@ namespace MiniGlobe.Core
         [Test]
         public void Add()
         {
-            Vector3D v1 = new Vector3D(1.0, 2.0, 3.0);
-            Vector3D v2 = new Vector3D(4.0, 5.0, 6.0);
+            Vector4D v1 = new Vector4D(1.0, 2.0, 3.0, 4.0);
+            Vector4D v2 = new Vector4D(4.0, 5.0, 6.0, 7.0);
             
-            Vector3D v3 = v1 + v2;
+            Vector4D v3 = v1 + v2;
             Assert.AreEqual(5.0, v3.X, 1e-14);
             Assert.AreEqual(7.0, v3.Y, 1e-14);
             Assert.AreEqual(9.0, v3.Z, 1e-14);
+            Assert.AreEqual(11.0, v3.W, 1e-14);
 
-            Vector3D v4 = v1.Add(v2);
+            Vector4D v4 = v1.Add(v2);
             Assert.AreEqual(5.0, v4.X, 1e-14);
             Assert.AreEqual(7.0, v4.Y, 1e-14);
             Assert.AreEqual(9.0, v4.Z, 1e-14);
+            Assert.AreEqual(11.0, v4.W, 1e-14);
         }
 
         [Test]
         public void Subtract()
         {
-            Vector3D v1 = new Vector3D(1.0, 2.0, 3.0);
-            Vector3D v2 = new Vector3D(4.0, 5.0, 6.0);
+            Vector4D v1 = new Vector4D(1.0, 2.0, 3.0, 4.0);
+            Vector4D v2 = new Vector4D(4.0, 5.0, 6.0, 7.0);
 
-            Vector3D v3 = v1 - v2;
+            Vector4D v3 = v1 - v2;
             Assert.AreEqual(-3.0, v3.X, 1e-14);
             Assert.AreEqual(-3.0, v3.Y, 1e-14);
             Assert.AreEqual(-3.0, v3.Z, 1e-14);
+            Assert.AreEqual(-3.0, v3.W, 1e-14);
 
-            Vector3D v4 = v1.Subtract(v2);
+            Vector4D v4 = v1.Subtract(v2);
             Assert.AreEqual(-3.0, v4.X, 1e-14);
             Assert.AreEqual(-3.0, v4.Y, 1e-14);
             Assert.AreEqual(-3.0, v4.Z, 1e-14);
+            Assert.AreEqual(-3.0, v4.W, 1e-14);
         }
 
         [Test]
         public void Multiply()
         {
-            Vector3D v1 = new Vector3D(1.0, 2.0, 3.0);
+            Vector4D v1 = new Vector4D(1.0, 2.0, 3.0, 4.0);
 
-            Vector3D v2 = v1 * 5.0;
+            Vector4D v2 = v1 * 5.0;
             Assert.AreEqual(5.0, v2.X, 1e-14);
             Assert.AreEqual(10.0, v2.Y, 1e-14);
             Assert.AreEqual(15.0, v2.Z, 1e-14);
+            Assert.AreEqual(20.0, v2.W, 1e-14);
 
-            Vector3D v3 = 5.0 * v1;
+            Vector4D v3 = 5.0 * v1;
             Assert.AreEqual(5.0, v3.X, 1e-14);
             Assert.AreEqual(10.0, v3.Y, 1e-14);
             Assert.AreEqual(15.0, v3.Z, 1e-14);
+            Assert.AreEqual(20.0, v3.W, 1e-14);
 
-            Vector3D v4 = v1.Multiply(5.0);
+            Vector4D v4 = v1.Multiply(5.0);
             Assert.AreEqual(5.0, v4.X, 1e-14);
             Assert.AreEqual(10.0, v4.Y, 1e-14);
             Assert.AreEqual(15.0, v4.Z, 1e-14);
+            Assert.AreEqual(20.0, v4.W, 1e-14);
         }
 
         [Test]
         public void MultiplyComponents()
         {
-            Vector3D v1 = new Vector3D(1, 2, 3);
-            Vector3D v2 = new Vector3D(4, 8, 12);
+            Vector4D v1 = new Vector4D(1, 2, 3, 4);
+            Vector4D v2 = new Vector4D(4, 8, 12, 16);
 
-            Assert.AreEqual(new Vector3D(4, 16, 36), v1.MultiplyComponents(v2));
+            Assert.AreEqual(new Vector4D(4, 16, 36, 64), v1.MultiplyComponents(v2));
         }
 
         [Test]
         public void Divide()
         {
-            Vector3D v1 = new Vector3D(10.0, 20.0, 30.0);
+            Vector4D v1 = new Vector4D(10.0, 20.0, 30.0, 40.0);
 
-            Vector3D v2 = v1 / 5.0;
+            Vector4D v2 = v1 / 5.0;
             Assert.AreEqual(2.0, v2.X, 1e-14);
             Assert.AreEqual(4.0, v2.Y, 1e-14);
             Assert.AreEqual(6.0, v2.Z, 1e-14);
+            Assert.AreEqual(8.0, v2.W, 1e-14);
 
-            Vector3D v3 = v1.Divide(5.0);
+            Vector4D v3 = v1.Divide(5.0);
             Assert.AreEqual(2.0, v3.X, 1e-14);
             Assert.AreEqual(4.0, v3.Y, 1e-14);
             Assert.AreEqual(6.0, v3.Z, 1e-14);
+            Assert.AreEqual(8.0, v3.W, 1e-14);
         }
 
         [Test]
         public void MostOrthogonalAxis()
         {
-            Vector3D v1 = new Vector3D(1, 2, 3);
-            Assert.AreEqual(Vector3D.UnitX, v1.MostOrthogonalAxis);
+            Vector4D v1 = new Vector4D(1, 2, 3, 4);
+            Assert.AreEqual(Vector4D.UnitX, v1.MostOrthogonalAxis);
 
-            Vector3D v2 = new Vector3D(-3, -1, -2);
-            Assert.AreEqual(Vector3D.UnitY, v2.MostOrthogonalAxis);
+            Vector4D v2 = new Vector4D(-3, -1, -2, -4);
+            Assert.AreEqual(Vector4D.UnitY, v2.MostOrthogonalAxis);
 
-            Vector3D v3 = new Vector3D(3, 2, 1);
-            Assert.AreEqual(Vector3D.UnitZ, v3.MostOrthogonalAxis);
+            Vector4D v3 = new Vector4D(3, 2, 1, 4);
+            Assert.AreEqual(Vector4D.UnitZ, v3.MostOrthogonalAxis);
+
+            Vector4D v4 = new Vector4D(3, 2, 4, 1);
+            Assert.AreEqual(Vector4D.UnitW, v4.MostOrthogonalAxis);
         }
 
         [Test]
         public void Zero()
         {
-            Assert.AreEqual(0.0, Vector3D.Zero.X);
-            Assert.AreEqual(0.0, Vector3D.Zero.Y);
-            Assert.AreEqual(0.0, Vector3D.Zero.Z);
+            Assert.AreEqual(0.0, Vector4D.Zero.X);
+            Assert.AreEqual(0.0, Vector4D.Zero.Y);
+            Assert.AreEqual(0.0, Vector4D.Zero.Z);
+            Assert.AreEqual(0.0, Vector4D.Zero.W);
         }
 
         [Test]
         public void UnitX()
         {
-            Assert.AreEqual(1.0, Vector3D.UnitX.X);
-            Assert.AreEqual(0.0, Vector3D.UnitX.Y);
-            Assert.AreEqual(0.0, Vector3D.UnitX.Z);
+            Assert.AreEqual(1.0, Vector4D.UnitX.X);
+            Assert.AreEqual(0.0, Vector4D.UnitX.Y);
+            Assert.AreEqual(0.0, Vector4D.UnitX.Z);
+            Assert.AreEqual(0.0, Vector4D.UnitX.W);
         }
 
         [Test]
         public void UnitY()
         {
-            Assert.AreEqual(0.0, Vector3D.UnitY.X);
-            Assert.AreEqual(1.0, Vector3D.UnitY.Y);
-            Assert.AreEqual(0.0, Vector3D.UnitY.Z);
+            Assert.AreEqual(0.0, Vector4D.UnitY.X);
+            Assert.AreEqual(1.0, Vector4D.UnitY.Y);
+            Assert.AreEqual(0.0, Vector4D.UnitY.Z);
+            Assert.AreEqual(0.0, Vector4D.UnitY.W);
         }
 
         [Test]
         public void UnitZ()
         {
-            Assert.AreEqual(0.0, Vector3D.UnitZ.X);
-            Assert.AreEqual(0.0, Vector3D.UnitZ.Y);
-            Assert.AreEqual(1.0, Vector3D.UnitZ.Z);
+            Assert.AreEqual(0.0, Vector4D.UnitZ.X);
+            Assert.AreEqual(0.0, Vector4D.UnitZ.Y);
+            Assert.AreEqual(1.0, Vector4D.UnitZ.Z);
+            Assert.AreEqual(0.0, Vector4D.UnitZ.W);
+        }
+
+        [Test]
+        public void UnitW()
+        {
+            Assert.AreEqual(0.0, Vector4D.UnitW.X);
+            Assert.AreEqual(0.0, Vector4D.UnitW.Y);
+            Assert.AreEqual(0.0, Vector4D.UnitW.Z);
+            Assert.AreEqual(1.0, Vector4D.UnitW.W);
         }
 
         [Test]
         public void Undefined()
         {
-            Assert.IsNaN(Vector3D.Undefined.X);
-            Assert.IsNaN(Vector3D.Undefined.Y);
-            Assert.IsNaN(Vector3D.Undefined.Z);
+            Assert.IsNaN(Vector4D.Undefined.X);
+            Assert.IsNaN(Vector4D.Undefined.Y);
+            Assert.IsNaN(Vector4D.Undefined.Z);
+            Assert.IsNaN(Vector4D.Undefined.W);
         }
 
         [Test]
         public void IsUndefined()
         {
-            Assert.IsTrue(Vector3D.Undefined.IsUndefined);
-            Assert.IsFalse(Vector3D.UnitX.IsUndefined);
-            Assert.IsFalse(Vector3D.UnitY.IsUndefined);
-            Assert.IsFalse(Vector3D.UnitZ.IsUndefined);
+            Assert.IsTrue(Vector4D.Undefined.IsUndefined);
+            Assert.IsFalse(Vector4D.UnitX.IsUndefined);
+            Assert.IsFalse(Vector4D.UnitY.IsUndefined);
+            Assert.IsFalse(Vector4D.UnitZ.IsUndefined);
+            Assert.IsFalse(Vector4D.UnitW.IsUndefined);
         }
 
         [Test]
         public void TestEquals()
         {
-            Vector3D a = new Vector3D(1.0, 2.0, 3.0);
-            Vector3D b = new Vector3D(4.0, 5.0, 6.0);
-            Vector3D c = new Vector3D(1.0, 2.0, 3.0);
+            Vector4D a = new Vector4D(1.0, 2.0, 3.0, 4.0);
+            Vector4D b = new Vector4D(4.0, 5.0, 6.0, 7.0);
+            Vector4D c = new Vector4D(1.0, 2.0, 3.0, 4.0);
 
             Assert.IsTrue(a.Equals(c));
             Assert.IsTrue(c.Equals(a));
@@ -267,9 +305,9 @@ namespace MiniGlobe.Core
         [Test]
         public void TestGetHashCode()
         {
-            Vector3D a = new Vector3D(1.0, 2.0, 3.0);
-            Vector3D b = new Vector3D(4.0, 5.0, 6.0);
-            Vector3D c = new Vector3D(1.0, 2.0, 3.0);
+            Vector4D a = new Vector4D(1.0, 2.0, 3.0, 4.0);
+            Vector4D b = new Vector4D(4.0, 5.0, 6.0, 7.0);
+            Vector4D c = new Vector4D(1.0, 2.0, 3.0, 4.0);
 
             Assert.AreEqual(a.GetHashCode(), c.GetHashCode());
             Assert.AreNotEqual(a.GetHashCode(), b.GetHashCode());
