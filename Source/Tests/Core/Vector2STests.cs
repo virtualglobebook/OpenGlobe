e6744d559bfa754ed4772fa2d01ea4d369616c94
kring789 <kring789@6eae0abb-6483-4171-a68c-a7d7e9ebe9e5>
2010-03-14 03:48:41 +0000

    Branch Vector2DTests to Vector2STests.

---
+++ Source/Tests/Core/Vector2STests.cs
@@ -0,0 +1,209 @@
+﻿#region License
+//
+// (C) Copyright 2010 Patrick Cozzi, Deron Ohlarik, and Kevin Ring
+//
+// Distributed under the Boost Software License, Version 1.0.
+// See License.txt or http://www.boost.org/LICENSE_1_0.txt.
+//
+#endregion
+
+using NUnit.Framework;
+
+namespace MiniGlobe.Core
+{
+    [TestFixture]
+    public class Vector2DTests
+    {
+        [Test]
+        public void Construct()
+        {
+            Vector2D v = new Vector2D(1.0, 2.0);
+            Assert.AreEqual(1.0, v.X);
+            Assert.AreEqual(2.0, v.Y);
+        }
+
+        [Test]
+        public void Magnitude()
+        {
+            Vector2D v = new Vector2D(3.0, 4.0);
+            Assert.AreEqual(25.0, v.MagnitudeSquared, 1e-14);
+            Assert.AreEqual(5.0, v.Magnitude, 1e-14);
+        }
+
+        [Test]
+        public void Normalize()
+        {
+            Vector2D v, n1, n2;
+            double magnitude;
+
+            v = new Vector2D(3.0, 4.0);
+            n1 = v.Normalize();
+            n2 = v.Normalize(out magnitude);
+            Assert.AreEqual(1.0, n1.Magnitude, 1e-14);
+            Assert.AreEqual(1.0, n2.Magnitude, 1e-14);
+            Assert.AreEqual(5.0, magnitude, 1e-14);
+        }
+
+        [Test]
+        public void NormalizeZeroVector()
+        {
+            Vector2D v = new Vector2D(0.0, 0.0);
+
+            Vector2D n1 = v.Normalize();
+            Assert.IsNaN(n1.X);
+            Assert.IsNaN(n1.Y);
+            Assert.IsTrue(n1.IsUndefined);
+
+            double magnitude;
+            Vector2D n2 = v.Normalize(out magnitude);
+            Assert.IsNaN(n2.X);
+            Assert.IsNaN(n2.Y);
+            Assert.IsTrue(n2.IsUndefined);
+            Assert.AreEqual(0.0, magnitude);
+        }
+
+        [Test]
+        public void Add()
+        {
+            Vector2D v1 = new Vector2D(1.0, 2.0);
+            Vector2D v2 = new Vector2D(4.0, 5.0);
+
+            Vector2D v3 = v1 + v2;
+            Assert.AreEqual(5.0, v3.X, 1e-14);
+            Assert.AreEqual(7.0, v3.Y, 1e-14);
+
+            Vector2D v4 = v1.Add(v2);
+            Assert.AreEqual(5.0, v4.X, 1e-14);
+            Assert.AreEqual(7.0, v4.Y, 1e-14);
+        }
+
+        [Test]
+        public void Subtract()
+        {
+            Vector2D v1 = new Vector2D(1.0, 2.0);
+            Vector2D v2 = new Vector2D(4.0, 5.0);
+
+            Vector2D v3 = v1 - v2;
+            Assert.AreEqual(-3.0, v3.X, 1e-14);
+            Assert.AreEqual(-3.0, v3.Y, 1e-14);
+
+            Vector2D v4 = v1.Subtract(v2);
+            Assert.AreEqual(-3.0, v4.X, 1e-14);
+            Assert.AreEqual(-3.0, v4.Y, 1e-14);
+        }
+
+        [Test]
+        public void Multiply()
+        {
+            Vector2D v1 = new Vector2D(1.0, 2.0);
+
+            Vector2D v2 = v1 * 5.0;
+            Assert.AreEqual(5.0, v2.X, 1e-14);
+            Assert.AreEqual(10.0, v2.Y, 1e-14);
+
+            Vector2D v3 = 5.0 * v1;
+            Assert.AreEqual(5.0, v3.X, 1e-14);
+            Assert.AreEqual(10.0, v3.Y, 1e-14);
+
+            Vector2D v4 = v1.Multiply(5.0);
+            Assert.AreEqual(5.0, v4.X, 1e-14);
+            Assert.AreEqual(10.0, v4.Y, 1e-14);
+        }
+
+        [Test]
+        public void Divide()
+        {
+            Vector2D v1 = new Vector2D(10.0, 20.0);
+
+            Vector2D v2 = v1 / 5.0;
+            Assert.AreEqual(2.0, v2.X, 1e-14);
+            Assert.AreEqual(4.0, v2.Y, 1e-14);
+
+            Vector2D v3 = v1.Divide(5.0);
+            Assert.AreEqual(2.0, v3.X, 1e-14);
+            Assert.AreEqual(4.0, v3.Y, 1e-14);
+        }
+
+        [Test]
+        public void Zero()
+        {
+            Assert.AreEqual(0.0, Vector2D.Zero.X);
+            Assert.AreEqual(0.0, Vector2D.Zero.Y);
+        }
+
+        [Test]
+        public void UnitX()
+        {
+            Assert.AreEqual(1.0, Vector2D.UnitX.X);
+            Assert.AreEqual(0.0, Vector2D.UnitX.Y);
+        }
+
+        [Test]
+        public void UnitY()
+        {
+            Assert.AreEqual(0.0, Vector2D.UnitY.X);
+            Assert.AreEqual(1.0, Vector2D.UnitY.Y);
+        }
+
+        [Test]
+        public void Undefined()
+        {
+            Assert.IsNaN(Vector2D.Undefined.X);
+            Assert.IsNaN(Vector2D.Undefined.Y);
+        }
+
+        [Test]
+        public void IsUndefined()
+        {
+            Assert.IsTrue(Vector2D.Undefined.IsUndefined);
+            Assert.IsFalse(Vector2D.UnitX.IsUndefined);
+            Assert.IsFalse(Vector2D.UnitY.IsUndefined);
+        }
+
+        [Test]
+        public void TestEquals()
+        {
+            Vector2D a = new Vector2D(1.0, 2.0);
+            Vector2D b = new Vector2D(4.0, 5.0);
+            Vector2D c = new Vector2D(1.0, 2.0);
+
+            Assert.IsTrue(a.Equals(c));
+            Assert.IsTrue(c.Equals(a));
+            Assert.IsTrue(a == c);
+            Assert.IsTrue(c == a);
+            Assert.IsFalse(c != a);
+            Assert.IsFalse(c != a);
+            Assert.IsFalse(a.Equals(b));
+            Assert.IsFalse(b.Equals(a));
+            Assert.IsFalse(a == b);
+            Assert.IsFalse(b == a);
+            Assert.IsTrue(a != b);
+            Assert.IsTrue(b != a);
+
+            object objA = a;
+            object objB = b;
+            object objC = c;
+
+            Assert.IsTrue(a.Equals(objA));
+            Assert.IsTrue(a.Equals(objC));
+            Assert.IsFalse(a.Equals(objB));
+
+            Assert.IsTrue(objA.Equals(objC));
+            Assert.IsFalse(objA.Equals(objB));
+
+            Assert.IsFalse(a.Equals(null));
+            Assert.IsFalse(a.Equals(5));
+        }
+
+        [Test]
+        public void TestGetHashCode()
+        {
+            Vector2D a = new Vector2D(1.0, 2.0);
+            Vector2D b = new Vector2D(4.0, 5.0);
+            Vector2D c = new Vector2D(1.0, 2.0);
+
+            Assert.AreEqual(a.GetHashCode(), c.GetHashCode());
+            Assert.AreNotEqual(a.GetHashCode(), b.GetHashCode());
+        }
+    }
+}
