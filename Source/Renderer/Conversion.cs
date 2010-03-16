1d9da7a2b5ef0ef005ed3c7fc4bd3bc6a83bdf32
kring789 <kring789@6eae0abb-6483-4171-a68c-a7d7e9ebe9e5>
2010-03-16 03:12:31 +0000

    Branch Core/Conversion.cs into Renderer instead.  Soon this class will go away entirely.

---
+++ Source/Renderer/Conversion.cs
@@ -0,0 +1,25 @@
+﻿#region License
+//
+// (C) Copyright 2010 Patrick Cozzi and Deron Ohlarik
+//
+// Distributed under the Boost Software License, Version 1.0.
+// See License.txt or http://www.boost.org/LICENSE_1_0.txt.
+//
+#endregion
+
+using OpenTK;
+
+namespace MiniGlobe.Core
+{
+    public static class Conversion
+    {
+        public static Matrix4 ToMatrix4(Matrix4d matrix)
+        {
+            return new Matrix4(
+                (float)matrix.M11, (float)matrix.M12, (float)matrix.M13, (float)matrix.M14,
+                (float)matrix.M21, (float)matrix.M22, (float)matrix.M23, (float)matrix.M24,
+                (float)matrix.M31, (float)matrix.M32, (float)matrix.M33, (float)matrix.M34,
+                (float)matrix.M41, (float)matrix.M42, (float)matrix.M43, (float)matrix.M44);
+        }
+    }
+}
