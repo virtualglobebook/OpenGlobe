61c74724a17313c6f66aaf32e0ba8e4d6e66e80f
pjcozzi <pjcozzi@6eae0abb-6483-4171-a68c-a7d7e9ebe9e5>
2010-04-05 23:01:42 +0000

    Added skeleton project for depth buffer precision section.

---
+++ Source/Examples/Chapter4/DepthBufferPrecision/DepthBufferPrecision.cs
@@ -0,0 +1,92 @@
+﻿#region License
+//
+// (C) Copyright 2010 Patrick Cozzi and Deron Ohlarik
+//
+// Distributed under the Boost Software License, Version 1.0.
+// See License.txt or http://www.boost.org/LICENSE_1_0.txt.
+//
+#endregion
+
+using System;
+using System.Drawing;
+
+using MiniGlobe.Core.Geometry;
+using MiniGlobe.Renderer;
+using MiniGlobe.Scene;
+
+using MiniGlobe.Core;
+using MiniGlobe.Terrain;
+
+namespace MiniGlobe.Examples.Chapter5
+{
+    sealed class DepthBufferPrecision : IDisposable
+    {
+        public DepthBufferPrecision()
+        {
+            _window = Device.CreateWindow(800, 600, "Chapter 4:  Depth Buffer Precision");
+            _window.Resize += OnResize;
+            _window.RenderFrame += OnRenderFrame;
+            _sceneState = new SceneState();
+
+            _sceneState.Camera.PerspectiveFarPlaneDistance = 1;
+            _sceneState.Camera.PerspectiveFarPlaneDistance = 4096;
+
+            ///////////////////////////////////////////////////////////////////
+
+            Ellipsoid globeShape = Ellipsoid.UnitSphere;
+            _globe = new TessellatedGlobe(_window.Context);
+            _globe.Shape = globeShape;
+            _globe.Texture = Device.CreateTexture2D(new Bitmap("MapperWDB.jpg"), TextureFormat.RedGreenBlue8, false);
+
+            _camera = new CameraLookAtPoint(_sceneState.Camera, _window, globeShape);
+            _sceneState.Camera.ZoomToTarget(globeShape.MaximumRadius);
+
+            HighResolutionSnap snap = new HighResolutionSnap(_window, _sceneState);
+            snap.ColorFilename = @"E:\Dropbox\My Dropbox\Book\Manuscript\DepthBufferPrecision\Figures\DepthBufferPrecision.png";
+            snap.WidthInInches = 3;
+            snap.DotsPerInch = 600;
+        }
+
+        private void OnResize()
+        {
+            _window.Context.Viewport = new Rectangle(0, 0, _window.Width, _window.Height);
+            _sceneState.Camera.AspectRatio = _window.Width / (double)_window.Height;
+        }
+
+        private void OnRenderFrame()
+        {
+            _window.Context.Clear(ClearBuffers.ColorAndDepthBuffer, Color.White, 1, 0);
+
+            _globe.Render(_sceneState);
+        }
+
+        #region IDisposable Members
+
+        public void Dispose()
+        {
+            _window.Dispose();
+            _camera.Dispose();
+            _globe.Dispose();
+        }
+
+        #endregion
+
+        private void Run(double updateRate)
+        {
+            _window.Run(updateRate);
+        }
+
+        static void Main()
+        {
+            using (DepthBufferPrecision example = new DepthBufferPrecision())
+            {
+                example.Run(30.0);
+            }
+        }
+
+        private readonly MiniGlobeWindow _window;
+        private readonly SceneState _sceneState;
+        private readonly CameraLookAtPoint _camera;
+        private readonly TessellatedGlobe _globe;
+    }
+}
---
+++ Source/Examples/Chapter4/DepthBufferPrecision/Properties/AssemblyInfo.cs
@@ -0,0 +1,30 @@
+﻿#region License
+//
+// (C) Copyright 2010 Patrick Cozzi and Deron Ohlarik
+//
+// Distributed under the Boost Software License, Version 1.0.
+// See License.txt or http://www.boost.org/LICENSE_1_0.txt.
+//
+#endregion
+
+using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MiniGlobe.Examples.Chapter5.DepthBufferPrecision")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MiniGlobe.Examples.Chapter5.DepthBufferPrecision")]
+[assembly: AssemblyCopyright("Copyright ©  2010")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: CLSCompliant(true)]
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5ebbc3b7-6c63-477b-ba1d-dad5e2c69bf4")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
