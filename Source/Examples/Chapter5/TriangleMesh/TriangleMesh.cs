b299f68ecf13f0c3ad089cf08de35810cf101674
pjcozzi <pjcozzi@6eae0abb-6483-4171-a68c-a7d7e9ebe9e5>
2010-04-23 21:55:52 +0000

    Skeleton project for terrain rendering with a simple triangle mesh.

---
+++ Source/Examples/Chapter5/TriangleMesh/TriangleMesh.cs
@@ -0,0 +1,100 @@
+﻿#region License
+//
+// (C) Copyright 2010 Patrick Cozzi and Deron Ohlarik
+//
+// Distributed under the Boost Software License, Version 1.0.
+// See License.txt or http://www.boost.org/LICENSE_1_0.txt.
+//
+#endregion
+
+using System;
+using System.Drawing;
+
+using MiniGlobe.Core.Geometry;
+using MiniGlobe.Renderer;
+using MiniGlobe.Scene;
+
+using MiniGlobe.Core;
+using MiniGlobe.Terrain;
+
+namespace MiniGlobe.Examples.Chapter5
+{
+    sealed class TriangleMesh : IDisposable
+    {
+        public TriangleMesh()
+        {
+            _window = Device.CreateWindow(800, 600, "Chapter 5:  Triangle Mesh");
+            _window.Resize += OnResize;
+            _window.RenderFrame += OnRenderFrame;
+            _sceneState = new SceneState();
+            _sceneState.Camera.PerspectiveFarPlaneDistance = 4096;
+
+            ///////////////////////////////////////////////////////////////////
+
+            TerrainTile terrainTile = TerrainTile.FromBitmap(new Bitmap(@"ps-e.lg.jpg"));
+            _tile = new VertexDisplacementMapTerrainTile(_window.Context, terrainTile);
+            _tile.HeightExaggeration = 30;
+
+            ///////////////////////////////////////////////////////////////////
+
+            _axes = new Axes(_window.Context);
+            _axes.Length = 25;
+            _axes.Width = 3;
+
+            double tileRadius = Math.Max(terrainTile.Size.X, terrainTile.Size.Y) * 0.5;
+            _camera = new CameraLookAtPoint(_sceneState.Camera, _window, Ellipsoid.UnitSphere);
+            _camera.CenterPoint = new Vector3D(terrainTile.Size.X * 0.5, terrainTile.Size.Y * 0.5, 0.0);
+            _sceneState.Camera.ZoomToTarget(tileRadius);
+            
+            HighResolutionSnap snap = new HighResolutionSnap(_window, _sceneState);
+            snap.ColorFilename = @"E:\Manuscript\TerrainRendering\Figures\TriangleMesh.png";
+            snap.WidthInInches = 3;
+            snap.DotsPerInch = 600;
+        }
+
+        private void OnResize()
+        {
+            _window.Context.Viewport = new Rectangle(0, 0, _window.Width, _window.Height);
+            _sceneState.Camera.AspectRatio = _window.Width / (double)_window.Height;
+        }
+
+        private void OnRenderFrame()
+        {
+            _window.Context.Clear(ClearBuffers.ColorAndDepthBuffer, Color.White, 1, 0);
+
+            _tile.Render(_sceneState);
+            _axes.Render(_sceneState);
+        }
+
+        #region IDisposable Members
+
+        public void Dispose()
+        {
+            _window.Dispose();
+            _camera.Dispose();
+            _tile.Dispose();
+            _axes.Dispose();
+        }
+
+        #endregion
+
+        private void Run(double updateRate)
+        {
+            _window.Run(updateRate);
+        }
+
+        static void Main()
+        {
+            using (TriangleMesh example = new TriangleMesh())
+            {
+                example.Run(30.0);
+            }
+        }
+
+        private readonly MiniGlobeWindow _window;
+        private readonly SceneState _sceneState;
+        private readonly CameraLookAtPoint _camera;
+        private readonly VertexDisplacementMapTerrainTile _tile;
+        private readonly Axes _axes;
+    }
+}
