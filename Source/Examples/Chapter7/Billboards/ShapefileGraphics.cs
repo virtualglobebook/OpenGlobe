a2ee1fb5e8142ec223539a8074f1062d464ddfd0
pjcozzi <pjcozzi@6eae0abb-6483-4171-a68c-a7d7e9ebe9e5>
2010-05-25 16:00:17 +0000

    Incomplete class to render shape files.

---
+++ Source/Examples/Chapter7/Billboards/ShapefileGraphics.cs
@@ -0,0 +1,73 @@
+﻿#region License
+//
+// (C) Copyright 2010 Patrick Cozzi and Deron Ohlarik
+//
+// Distributed under the Boost Software License, Version 1.0.
+// See License.txt or http://www.boost.org/LICENSE_1_0.txt.
+//
+#endregion
+
+using System;
+using System.Diagnostics;
+using MiniGlobe.Core;
+using MiniGlobe.Core.Geometry;
+using MiniGlobe.Renderer;
+using Catfood.Shapefile;
+
+namespace MiniGlobe.Examples.Chapter7
+{
+    public class ShapefileGraphics : IDisposable
+    {
+        public ShapefileGraphics(Context context, Ellipsoid globeShape, string filename)
+        {
+            _context = context;
+
+            // TODO: capacities
+
+            using (Shapefile shapefile = new Shapefile(filename))
+            {
+                foreach (Shape shape in shapefile)
+                {
+                    switch (shape.Type)
+                    {
+                        case ShapeType.PolyLine:
+
+                            ShapePolyLine shapePolyline = shape as ShapePolyLine;
+
+                            foreach (PointD[] part in shapePolyline.Parts)
+                            {
+                                foreach (PointD point in part)
+                                {
+                                    globeShape.ToVector3D(Trig.ToRadians(new Geodetic3D(point.X, point.Y)));
+                                }
+                                Console.WriteLine();
+                            }
+                            break;
+
+                        default:
+                            Debug.Fail("Not Implemented");
+                    }
+                }
+            }
+        }
+
+        public void Render(SceneState sceneState)
+        {
+        }
+
+        public Context Context
+        {
+            get { return _context; }
+        }
+
+        #region IDisposable Members
+
+        public void Dispose()
+        {
+        }
+
+        #endregion
+
+        private Context _context;
+    }
+}
