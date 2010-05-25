1ac34ea0d89b19391a51f541829b321c43bfe001
pjcozzi <pjcozzi@6eae0abb-6483-4171-a68c-a7d7e9ebe9e5>
2010-05-25 17:24:24 +0000

    Added polyline support to shapefile rendering.

--- Source/Examples/Chapter7/Billboards/ShapefileGraphics.cs
+++ Source/Examples/Chapter7/Billboards/ShapefileGraphics.cs
@@ -8,10 +8,12 @@
 #endregion
 
 using System;
-using System.Diagnostics;
+using System.Drawing;
+using System.Collections.Generic;
 using MiniGlobe.Core;
 using MiniGlobe.Core.Geometry;
 using MiniGlobe.Renderer;
+using MiniGlobe.Scene;
 using Catfood.Shapefile;
 
 namespace MiniGlobe.Examples.Chapter7
@@ -20,39 +22,113 @@ namespace MiniGlobe.Examples.Chapter7
     {
         public ShapefileGraphics(Context context, Ellipsoid globeShape, string filename)
         {
-            _context = context;
+            if (context == null)
+            {
+                throw new ArgumentNullException("context");
+            }
 
-            // TODO: capacities
+            if (globeShape == null)
+            {
+                throw new ArgumentNullException("globeShape");
+            }
+
+            _context = context;
 
             using (Shapefile shapefile = new Shapefile(filename))
             {
-                foreach (Shape shape in shapefile)
+                if (shapefile.Type == ShapeType.PolyLine)
                 {
-                    switch (shape.Type)
-                    {
-                        case ShapeType.PolyLine:
-
-                            ShapePolyLine shapePolyline = shape as ShapePolyLine;
-
-                            foreach (PointD[] part in shapePolyline.Parts)
-                            {
-                                foreach (PointD point in part)
-                                {
-                                    globeShape.ToVector3D(Trig.ToRadians(new Geodetic3D(point.X, point.Y)));
-                                }
-                                Console.WriteLine();
-                            }
-                            break;
-
-                        default:
-                            Debug.Fail("Not Implemented");
-                    }
+                    _polyline = new OutlinedPolylineTexture(context);
+                    CreatePolylines(globeShape, shapefile);
+                }
+                else
+                {
+                    throw new NotSupportedException("Shapefile type is not supported.");
                 }
             }
         }
 
+        private void CreatePolylines(Ellipsoid globeShape, Shapefile shapefile)
+        {
+            int positionsCount = 0;
+            int indicesCount = 0;
+            //PolylineCapacities(shapefile, out positionsCount, out indicesCount);
+
+            VertexAttributeDoubleVector3 positionAttribute = new VertexAttributeDoubleVector3("position", positionsCount);
+            VertexAttributeRGBA colorAttribute = new VertexAttributeRGBA("color", positionsCount);
+            VertexAttributeRGBA outlineColorAttribute = new VertexAttributeRGBA("outlineColor", positionsCount);
+            IndicesInt32 indices = new IndicesInt32(indicesCount);
+
+            foreach (Shape shape in shapefile)
+            {
+                if (shape.Type != ShapeType.PolyLine)
+                {
+                    throw new NotSupportedException("The type of an individual shape does not match the Shapefile type.");
+                }
+
+                IList<PointD[]> parts = (shape as ShapePolyLine).Parts;
+
+                for (int j = 0; j < parts.Count; ++j)
+                {
+                    PointD[] part = parts[j];
+
+                    for (int i = 0; i < part.Length; ++i)
+                    {
+                        PointD point = part[i];
+
+                        positionAttribute.Values.Add(globeShape.ToVector3D(Trig.ToRadians(new Geodetic3D(point.X, point.Y))));
+                        colorAttribute.AddColor(Color.Yellow);
+                        outlineColorAttribute.AddColor(Color.Black);
+
+                        if (i != 0)
+                        {
+                            indices.Values.Add(positionAttribute.Values.Count - 2);
+                            indices.Values.Add(positionAttribute.Values.Count - 1);
+                        }
+                    }
+                }
+            }
+
+            Mesh mesh = new Mesh();
+            mesh.PrimitiveType = PrimitiveType.Lines;
+            mesh.Attributes.Add(positionAttribute);
+            mesh.Attributes.Add(colorAttribute);
+            mesh.Attributes.Add(outlineColorAttribute);
+            mesh.Indices = indices;
+            _polyline.Set(mesh);
+        }
+
+        private static void PolylineCapacities(Shapefile shapefile, out int positionsCount, out int indicesCount)
+        {
+            int numberOfPositions = 0;
+            int numberOfIndices = 0;
+
+            foreach (Shape shape in shapefile)
+            {
+                if (shape.Type != ShapeType.PolyLine)
+                {
+                    throw new NotSupportedException("The type of an individual shape does not match the Shapefile type.");
+                }
+
+                IList<PointD[]> parts = (shape as ShapePolyLine).Parts;
+
+                for (int i = 0; i < parts.Count; ++i)
+                {
+                    numberOfPositions += parts[i].Length;
+                    numberOfIndices += (parts[i].Length - 1) * 2;
+                }
+            }
+
+            positionsCount = numberOfPositions;
+            indicesCount = numberOfIndices;
+        }
+
         public void Render(SceneState sceneState)
         {
+            if (_polyline != null)
+            {
+                _polyline.Render(sceneState);
+            }
         }
 
         public Context Context
@@ -64,10 +140,15 @@ namespace MiniGlobe.Examples.Chapter7
 
         public void Dispose()
         {
+            if (_polyline != null)
+            {
+                _polyline.Dispose();
+            }
         }
 
         #endregion
 
-        private Context _context;
+        private readonly Context _context;
+        private readonly OutlinedPolylineTexture _polyline;
     }
 }
