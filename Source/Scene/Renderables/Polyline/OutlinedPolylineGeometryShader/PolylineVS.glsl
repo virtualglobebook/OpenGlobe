31c5c04c1b35a97eee4fd4b46c381eca0946d197
pjcozzi <pjcozzi@6eae0abb-6483-4171-a68c-a7d7e9ebe9e5>
2010-05-28 00:47:13 +0000

    Moved more Polyline shaders to embedded resources.

---
+++ Source/Scene/Renderables/Polyline/OutlinedPolylineGeometryShader/PolylineVS.glsl
@@ -0,0 +1,21 @@
+﻿#version 150
+//
+// (C) Copyright 2010 Patrick Cozzi and Deron Ohlarik
+//
+// Distributed under the Boost Software License, Version 1.0.
+// See License.txt or http://www.boost.org/LICENSE_1_0.txt.
+//
+
+in vec4 position;
+in vec4 color;
+in vec4 outlineColor;
+
+out vec4 gsColor;
+out vec4 gsOutlineColor;
+
+void main()                     
+{
+	gl_Position = position;
+	gsColor = color;
+	gsOutlineColor = outlineColor;
+}
---
+++ Source/Scene/Renderables/Polyline/Polyline/PolylineVS.glsl
@@ -0,0 +1,17 @@
+﻿#version 150
+//
+// (C) Copyright 2010 Patrick Cozzi and Deron Ohlarik
+//
+// Distributed under the Boost Software License, Version 1.0.
+// See License.txt or http://www.boost.org/LICENSE_1_0.txt.
+//
+
+in vec4 position;
+in vec4 color;
+out vec4 gsColor;
+
+void main()                     
+{
+	gl_Position = position;
+	gsColor = color;
+}
