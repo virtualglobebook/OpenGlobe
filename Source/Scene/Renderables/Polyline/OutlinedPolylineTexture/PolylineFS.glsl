f682f09d6b8efc7a49f74afa923690906288d933
pjcozzi <pjcozzi@6eae0abb-6483-4171-a68c-a7d7e9ebe9e5>
2010-05-28 00:27:58 +0000

    Moved some polyline shaders to embedded resources.

---
+++ Source/Scene/Renderables/Polyline/OutlinedPolylineTexture/PolylineFS.glsl
@@ -0,0 +1,25 @@
+﻿#version 150
+//
+// (C) Copyright 2010 Patrick Cozzi and Deron Ohlarik
+//
+// Distributed under the Boost Software License, Version 1.0.
+// See License.txt or http://www.boost.org/LICENSE_1_0.txt.
+//
+
+flat in vec4 fsColor;
+flat in vec4 fsOutlineColor;
+in vec2 fsTextureCoordinate;
+
+out vec4 fragmentColor;
+
+uniform sampler2D mg_texture0;
+
+void main()
+{
+    vec2 texel = texture(mg_texture0, fsTextureCoordinate).rg;
+    float fill = texel.r;
+    float alpha = texel.g;
+
+    vec4 color = mix(fsOutlineColor, fsColor, fill);
+    fragmentColor = vec4(color.rgb, color.a * alpha);
+}
---
+++ Source/Scene/Renderables/Polyline/OutlinedPolylineTexture/PolylineGS.glsl
@@ -0,0 +1,105 @@
+﻿#version 150 
+//
+// (C) Copyright 2010 Patrick Cozzi and Deron Ohlarik
+//
+// Distributed under the Boost Software License, Version 1.0.
+// See License.txt or http://www.boost.org/LICENSE_1_0.txt.
+//
+
+layout(lines) in;
+layout(triangle_strip, max_vertices = 4) out;
+
+in vec4 gsColor[];
+in vec4 gsOutlineColor[];
+
+flat out vec4 fsColor;
+flat out vec4 fsOutlineColor;
+out vec2 fsTextureCoordinate;
+
+uniform mat4 mg_modelViewPerspectiveProjectionMatrix;
+uniform mat4 mg_viewportTransformationMatrix;
+uniform mat4 mg_viewportOrthographicProjectionMatrix;
+uniform float mg_perspectiveNearPlaneDistance;
+uniform float u_distance;
+
+vec4 ClipToWindowCoordinates(vec4 v, mat4 viewportTransformationMatrix)
+{
+    v.xyz /= v.w;                                                        // normalized device coordinates
+    v.xyz = (viewportTransformationMatrix * vec4(v.xyz + 1.0, 1.0)).xyz; // windows coordinates
+    return v;
+}
+
+void ClipLineSegmentToNearPlane(
+    float nearPlaneDistance, 
+    mat4 modelViewPerspectiveProjectionMatrix,
+    vec4 modelP0, 
+    vec4 modelP1, 
+    out vec4 clipP0, 
+    out vec4 clipP1)
+{
+    clipP0 = modelViewPerspectiveProjectionMatrix * modelP0;
+    clipP1 = modelViewPerspectiveProjectionMatrix * modelP1;
+
+    float distanceToP0 = clipP0.z - nearPlaneDistance;
+    float distanceToP1 = clipP1.z - nearPlaneDistance;
+
+    if ((distanceToP0 * distanceToP1) < 0.0)
+    {
+        float t = distanceToP0 / (distanceToP0 - distanceToP1);
+        vec3 modelV = vec3(modelP0) + t * (vec3(modelP1) - vec3(modelP0));
+        vec4 clipV = modelViewPerspectiveProjectionMatrix * vec4(modelV, 1);
+
+        if (distanceToP0 < 0.0)
+        {
+            clipP0 = clipV;
+        }
+        else
+        {
+            clipP1 = clipV;
+        }
+    }
+}
+
+void main()
+{
+    vec4 clipP0;
+    vec4 clipP1;
+    ClipLineSegmentToNearPlane(mg_perspectiveNearPlaneDistance, 
+    mg_modelViewPerspectiveProjectionMatrix,
+    gl_in[0].gl_Position, gl_in[1].gl_Position, clipP0, clipP1);
+
+    vec4 windowP0 = ClipToWindowCoordinates(clipP0, mg_viewportTransformationMatrix);
+    vec4 windowP1 = ClipToWindowCoordinates(clipP1, mg_viewportTransformationMatrix);
+
+    vec2 direction = windowP1.xy - windowP0.xy;
+    vec2 normal = normalize(vec2(direction.y, -direction.x));
+
+    vec4 v0 = vec4(windowP0.xy - (normal * u_distance), windowP0.z, 1.0);
+    vec4 v1 = vec4(windowP1.xy - (normal * u_distance), windowP1.z, 1.0);
+    vec4 v2 = vec4(windowP0.xy + (normal * u_distance), windowP0.z, 1.0);
+    vec4 v3 = vec4(windowP1.xy + (normal * u_distance), windowP1.z, 1.0);
+
+    gl_Position = mg_viewportOrthographicProjectionMatrix * v0;
+    fsColor = gsColor[0];
+    fsOutlineColor = gsOutlineColor[0];
+    fsTextureCoordinate = vec2(0.0, 0.0);
+    EmitVertex();
+
+    gl_Position = mg_viewportOrthographicProjectionMatrix * v1;
+    fsColor = gsColor[0];
+    fsOutlineColor = gsOutlineColor[0];
+    fsTextureCoordinate = vec2(0.0, 1.0);
+    EmitVertex();
+
+    gl_Position = mg_viewportOrthographicProjectionMatrix * v2;
+    fsColor = gsColor[0];
+    fsOutlineColor = gsOutlineColor[0];
+    fsTextureCoordinate = vec2(1.0, 0.0);
+    EmitVertex();
+
+    gl_Position = mg_viewportOrthographicProjectionMatrix * v3;
+    fsColor = gsColor[0];
+    fsOutlineColor = gsOutlineColor[0];
+    fsTextureCoordinate = vec2(1.0, 1.0);
+    EmitVertex();
+}
