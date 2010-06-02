7339b2d8e9584d7d0d29ac6b79c96c4d8ac243d1
nderon <nderon@6eae0abb-6483-4171-a68c-a7d7e9ebe9e5>
2010-06-02 17:41:22 +0000

    Lines on terrain code.

--- Source/Examples/Chapter8/LinesOnTerrain/LinesOnTerrain.cs
+++ Source/Examples/Chapter8/LinesOnTerrain/LinesOnTerrain.cs
@@ -9,6 +9,7 @@
 
 using System;
 using System.Drawing;
+using System.Collections.Generic;
 
 using MiniGlobe.Core.Geometry;
 using MiniGlobe.Renderer;
@@ -28,18 +29,16 @@ namespace MiniGlobe.Examples.Chapter8
             _window.RenderFrame += OnRenderFrame;
             _sceneState = new SceneState();
             _sceneState.Camera.PerspectiveFarPlaneDistance = 4096;
+            _sceneState.Camera.PerspectiveNearPlaneDistance = 10;
 
             ///////////////////////////////////////////////////////////////////
 
             TerrainTile terrainTile = TerrainTile.FromBitmap(new Bitmap(@"ps-e.lg.jpg"));
             _tile = new TriangleMeshTerrainTile(_window.Context, terrainTile);
-            _tile.HeightExaggeration = 30;
+            const double heightExaggeration = 30.0;
+            _tile.HeightExaggeration = (float)heightExaggeration;
 
             ///////////////////////////////////////////////////////////////////
-
-            _axes = new Axes();
-            _axes.Length = 25;
-            _axes.Width = 3;
 
             double tileRadius = Math.Max(terrainTile.Size.X, terrainTile.Size.Y) * 0.5;
             _camera = new CameraLookAtPoint(_sceneState.Camera, _window, Ellipsoid.UnitSphere);
@@ -50,21 +49,252 @@ namespace MiniGlobe.Examples.Chapter8
             snap.ColorFilename = @"E:\Manuscript\TerrainRendering\Figures\LinesOnTerrain.png";
             snap.WidthInInches = 3;
             snap.DotsPerInch = 600;
+            
+            //
+            // Depth
+            //
+            CreateDepth();
+
+            //
+            // Line on terrain
+            //
+            // Pass-thru shader
+            //
+            string vs;
+            string fs;
+
+            vs =
+                @"#version 150
+
+                in vec3 position;
+                  
+                uniform mat4 mg_modelViewPerspectiveProjectionMatrix;
+
+                void main()
+                {
+                    gl_Position = mg_modelViewPerspectiveProjectionMatrix * vec4(position, 1.0);
+                }";
+            fs =
+                @"#version 150
+                 
+                out vec3 fragmentColor;
+
+                uniform sampler2D mg_texture0;
+                uniform vec2 mg_inverseViewportDimensions;
+
+                void main()
+                {
+                    vec2 of = mg_inverseViewportDimensions * gl_FragCoord.xy;
+                    float center = texture(mg_texture0, of).r;
+                    if (gl_FragCoord.z < center)
+                    {
+                        fragmentColor = vec3(1.0, 1.0, 0.0);
+                    }
+                    else
+                    {
+                        discard;
+                    }
+                }";
+            _passThruSP = Device.CreateShaderProgram(vs, fs);
+
+            vs =
+                @"#version 150
+
+                uniform mat4 mg_modelViewPerspectiveProjectionMatrix;
+
+                in vec3 position;
+
+                void main()
+                {
+                    gl_Position = mg_modelViewPerspectiveProjectionMatrix * vec4(position, 1.0);
+                }";
+#if false
+            fs =
+                @"#version 150
+                
+                uniform sampler2D mg_texture0;
+                uniform vec2 mg_inverseViewportDimensions;
+ 
+                out vec3 fragmentColor;
+
+                void main()
+                {
+                    vec2 of = mg_inverseViewportDimensions * gl_FragCoord.xy;
+                    float center = textureOffset(mg_texture0, of, ivec2(0, 0)).r;
+                    if (gl_FragCoord.z < center)
+                    {
+                        //
+                        // Fragment is above the terrain
+                        //
+                        float upperLeft = textureOffset(mg_texture0, of, ivec2(-1.0, 1.0)).r;
+                        float upperCenter = textureOffset(mg_texture0, of, ivec2(0.0, 1.0)).r;
+                        float upperRight = textureOffset(mg_texture0, of, ivec2(1.0, 1.0)).r;
+                        float left = textureOffset(mg_texture0, of, ivec2(-1.0, 0.0)).r;
+                        float right = textureOffset(mg_texture0, of, ivec2(1.0, 0.0)).r;
+                        float lowerLeft = textureOffset(mg_texture0, of, ivec2(-1.0, -1.0)).r;
+                        float lowerCenter = textureOffset(mg_texture0, of, ivec2(0.0, -1.0)).r;
+                        float lowerRight = textureOffset(mg_texture0, of, ivec2(1.0, -1.0)).r;
+                        if ((step(upperLeft, gl_FragCoord.z) > 0.0) ||
+                            (step(upperCenter, gl_FragCoord.z) > 0.0) ||
+                            (step(upperRight, gl_FragCoord.z) > 0.0) ||
+                            (step(left, gl_FragCoord.z) > 0.0) ||
+                            (step(right, gl_FragCoord.z) > 0.0) ||
+                            (step(lowerLeft, gl_FragCoord.z) > 0.0) ||
+                            (step(lowerCenter, gl_FragCoord.z) > 0.0) ||
+                            (step(lowerRight, gl_FragCoord.z) > 0.0))
+                        {
+                            fragmentColor = vec3(1.0, 1.0, 0.0);
+                        }
+                        else
+                        {
+                            discard;
+                        }
+                    }
+                    else
+                    {
+                        discard;
+                    }
+                }";
+#else
+            fs =
+                @"#version 150
+                
+                uniform sampler2D mg_texture0;
+                uniform vec2 mg_inverseViewportDimensions;
+ 
+                out vec3 fragmentColor;
+
+                void main()
+                {
+                    float invDepth = 1.0 / gl_FragCoord.z;
+                    vec2 dInvDepth = vec2(dFdx(invDepth), dFdy(invDepth));
+
+                    vec2 of = mg_inverseViewportDimensions * gl_FragCoord.xy;
+                    float center = texture(mg_texture0, of).r;
+                    if (gl_FragCoord.z < center)
+                    {
+                        //
+                        // Fragment is above the terrain
+                        //
+                        float upperLeft = textureOffset(mg_texture0, of, ivec2(-1.0, 1.0)).r;
+                        float upperCenter = textureOffset(mg_texture0, of, ivec2(0.0, 1.0)).r;
+                        float upperRight = textureOffset(mg_texture0, of, ivec2(1.0, 1.0)).r;
+                        float left = textureOffset(mg_texture0, of, ivec2(-1.0, 0.0)).r;
+                        float right = textureOffset(mg_texture0, of, ivec2(1.0, 0.0)).r;
+                        float lowerLeft = textureOffset(mg_texture0, of, ivec2(-1.0, -1.0)).r;
+                        float lowerCenter = textureOffset(mg_texture0, of, ivec2(0.0, -1.0)).r;
+                        float lowerRight = textureOffset(mg_texture0, of, ivec2(1.0, -1.0)).r;
+
+                        float upperLeftM = 1.0 / (invDepth - dInvDepth.x + dInvDepth.y);
+                        float upperCenterM = 1.0 / (invDepth + dInvDepth.y);
+                        float upperRightM = 1.0 / (invDepth + dInvDepth.x + dInvDepth.y);
+                        float leftM = 1.0 / (invDepth - dInvDepth.x);
+                        float rightM = 1.0 / (invDepth + dInvDepth.x);
+                        float lowerLeftM = 1.0 / (invDepth - dInvDepth.x - dInvDepth.y);
+                        float lowerCenterM = 1.0 / (invDepth - dInvDepth.y);
+                        float lowerRightM = 1.0 / (invDepth + dInvDepth.x - dInvDepth.y);
+
+                        if ((step(upperLeft, upperLeftM) > 0.0) ||
+                            (step(upperCenter, upperCenterM) > 0.0) ||
+                            (step(upperRight, upperRightM) > 0.0) ||
+                            (step(left, leftM) > 0.0) ||
+                            (step(right, rightM) > 0.0) ||
+                            (step(lowerLeft, lowerLeftM) > 0.0) ||
+                            (step(lowerCenter, lowerCenterM) > 0.0) ||
+                            (step(lowerRight, lowerRightM) > 0.0))
+                        {
+                            fragmentColor = vec3(1.0, 1.0, 0.0);
+                        }
+                        else
+                        {
+                            discard;
+                        }
+                    }
+                    else
+                    {
+                        discard;
+                    }
+                }";
+#endif
+            _lotSP = Device.CreateShaderProgram(vs, fs);
+
+            //
+            // Mesh
+            //
+            Mesh mesh = new Mesh();
+            mesh.PrimitiveType = PrimitiveType.TriangleStrip;
+
+            //
+            // Positions
+            //
+            const int numberOfLineSegments = 4;
+            const int numberOfPositions = 2 + numberOfLineSegments + numberOfLineSegments;
+            VertexAttributeDoubleVector3 positionsAttribute = new VertexAttributeDoubleVector3("position", numberOfPositions);
+            IList<Vector3D> positions = positionsAttribute.Values;
+            mesh.Attributes.Add(positionsAttribute);
+            const double temp = 1.2;
+            positions.Add(new Vector3D(0.0, 0.0, heightExaggeration * -temp));
+            positions.Add(new Vector3D(0.0, 0.0, heightExaggeration * temp));
+            positions.Add(new Vector3D(100.0, 100.0, heightExaggeration * -temp));
+            positions.Add(new Vector3D(100.0, 100.0, heightExaggeration * temp));
+            positions.Add(new Vector3D(200.0, 100.0, heightExaggeration * -temp));
+            positions.Add(new Vector3D(200.0, 100.0, heightExaggeration * temp));
+            positions.Add(new Vector3D(256.0, 256.0, heightExaggeration * -temp));
+            positions.Add(new Vector3D(256.0, 256.0, heightExaggeration * temp));
+            positions.Add(new Vector3D(512.0, 512.0, heightExaggeration * -temp));
+            positions.Add(new Vector3D(512.0, 512.0, heightExaggeration * temp));
+
+            //
+            // Vertex array
+            //
+            _lineVA = _window.Context.CreateVertexArray(mesh, _lotSP.VertexAttributes, BufferHint.StaticDraw);
+            _lineRenderState = new RenderState();
+            _lineRenderState.FacetCulling.Enabled = false;
+            _lineRenderState.DepthTest.Enabled = false;
+            _lineRenderState.DepthWrite = false;
         }
 
         private void OnResize()
         {
             _window.Context.Viewport = new Rectangle(0, 0, _window.Width, _window.Height);
             _sceneState.Camera.AspectRatio = _window.Width / (double)_window.Height;
+            CreateDepth();
         }
 
         private void OnRenderFrame()
         {
-            Context context = _window.Context;
-            context.Clear(ClearBuffers.ColorAndDepthBuffer, Color.White, 1, 0);
+            //
+            // Depth 
+            //
+            _window.Context.Bind(_fbo);
+            _tile.Render(_window.Context, _sceneState);
+            _window.Context.Clear(ClearBuffers.ColorAndDepthBuffer, Color.White, 1, 0);
+            _tile.Render(_window.Context, _sceneState);
+            _window.Context.Bind(null as FrameBuffer);
 
-            _tile.Render(context, _sceneState);
-            _axes.Render(context, _sceneState);
+            //
+            // Terrain
+            //
+            _window.Context.Clear(ClearBuffers.ColorAndDepthBuffer, Color.White, 1, 0);
+            _tile.Render(_window.Context, _sceneState);
+
+            //
+            // Line on terrain
+            //
+            _window.Context.Bind(_lineVA);
+            _window.Context.Bind(_lineRenderState);
+            _window.Context.TextureUnits[0].Texture2D = _depthTexture;
+            _window.Context.Bind(_lotSP);
+            _window.Context.Draw(PrimitiveType.TriangleStrip, _sceneState);
+
+            //
+            // Pass-thru line on terrain
+            //
+            if (_passThru)
+            {
+                _window.Context.Bind(_passThruSP);
+                _window.Context.Draw(PrimitiveType.TriangleStrip, _sceneState);
+            }
         }
 
         #region IDisposable Members
@@ -73,11 +303,24 @@ namespace MiniGlobe.Examples.Chapter8
         {
             _camera.Dispose();
             _tile.Dispose();
-            _axes.Dispose();
             _window.Dispose();
         }
 
         #endregion
+
+        private void CreateDepth()
+        {
+            Texture2DDescription t2dd = new Texture2DDescription(_window.Width, _window.Height, TextureFormat.Depth24);
+            _depthTexture = Device.CreateTexture2D(t2dd);
+            t2dd = new Texture2DDescription(_window.Width, _window.Height, TextureFormat.RedGreenBlue8);
+            _colorTexture = Device.CreateTexture2D(t2dd);
+            if (_fbo == null)
+            {
+                _fbo = _window.Context.CreateFrameBuffer();
+            }
+            _fbo.DepthAttachment = _depthTexture;
+            _fbo.ColorAttachments[0] = _colorTexture;
+        }
 
         private void Run(double updateRate)
         {
@@ -96,6 +339,14 @@ namespace MiniGlobe.Examples.Chapter8
         private readonly SceneState _sceneState;
         private readonly CameraLookAtPoint _camera;
         private readonly TriangleMeshTerrainTile _tile;
-        private readonly Axes _axes;
+
+        private bool _passThru = false;
+        Texture2D _depthTexture;
+        Texture2D _colorTexture;
+        FrameBuffer _fbo;
+        VertexArray _lineVA;
+        RenderState _lineRenderState;
+        private readonly ShaderProgram _lotSP;
+        private readonly ShaderProgram _passThruSP;
     }
 }
