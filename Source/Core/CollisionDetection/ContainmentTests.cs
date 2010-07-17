4272ef6599b6e44e183a874e21b92623a4698f7f
pjcozzi <pjcozzi@6eae0abb-6483-4171-a68c-a7d7e9ebe9e5>
2010-07-17 18:23:07 +0000

    Added skeletons for containment tests, e.g., point in triangle.

---
+++ Source/Core/CollisionDetection/ContainmentTests.cs
@@ -0,0 +1,19 @@
+﻿#region License
+//
+// (C) Copyright 2010 Patrick Cozzi and Deron Ohlarik
+//
+// Distributed under the Boost Software License, Version 1.0.
+// See License.txt or http://www.boost.org/LICENSE_1_0.txt.
+//
+#endregion
+
+namespace OpenGlobe.Core
+{
+    public static class ContainmentTests
+    {
+        public static bool PointInsideTriangle(Vector2D point, Vector2D p0, Vector2D p1, Vector2D p2)
+        {
+            return true;
+        }
+    }
+}
---
+++ Source/Tests/Core/CollisionDetection/ContainmentTestsTests.cs
@@ -0,0 +1,28 @@
+﻿#region License
+//
+// (C) Copyright 2010 Patrick Cozzi, Deron Ohlarik
+//
+// Distributed under the Boost Software License, Version 1.0.
+// See License.txt or http://www.boost.org/LICENSE_1_0.txt.
+//
+#endregion
+
+using System;
+using NUnit.Framework;
+
+namespace OpenGlobe.Core
+{
+    [TestFixture]
+    public class ContainmentTestsTests
+    {
+        [Test]
+        public void Inside()
+        {
+            Assert.IsTrue(ContainmentTests.PointInsideTriangle(
+                new Vector2D(0.25, 0.25),
+                new Vector2D(0, 0),
+                new Vector2D(1, 0),
+                new Vector2D(0, 1)));
+        }
+    }
+}
