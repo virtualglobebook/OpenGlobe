347b4616bcbc32882f2fed0f8ca0542a05cec3ea
pjcozzi <pjcozzi@6eae0abb-6483-4171-a68c-a7d7e9ebe9e5>
2010-07-17 17:20:16 +0000

    Code and tests to compute the area and winding order of a 2D simple polygon.

---
+++ Source/Core/Polygons/SimplePolygonAlgorithms.cs
@@ -0,0 +1,69 @@
+﻿#region License
+//
+// (C) Copyright 2010 Patrick Cozzi and Deron Ohlarik
+//
+// Distributed under the Boost Software License, Version 1.0.
+// See License.txt or http://www.boost.org/LICENSE_1_0.txt.
+//
+#endregion
+
+using System.Collections.Generic;
+using System;
+
+namespace OpenGlobe.Core
+{
+    public enum PolygonWindingOrder
+    {
+        Clockwise,
+        Counterclockwise
+    }
+
+    public static class SimplePolygonAlgorithms
+    {
+        public static double ComputeArea(IEnumerable<Vector2D> positions)
+        {
+            if (positions == null)
+            {
+                throw new ArgumentNullException("positions");
+            }
+
+            int count = CollectionAlgorithms.EnumerableCount(positions);
+
+            if (count < 3)
+            {
+                throw new ArgumentOutOfRangeException("positions", "At least three positions are required.");
+            }
+
+            //
+            // Compute the area of the polygon.  The sign of the area determines the winding order.
+            //
+            double area = 0.0;
+            bool first = true;
+            Vector2D firstPosition = Vector2D.Zero;
+            Vector2D previousPosition = Vector2D.Zero;
+
+            foreach (Vector2D position in positions)
+            {
+                if (first)
+                {
+                    firstPosition = position;
+                    first = false;
+                }
+                else
+                {
+                    area += (previousPosition.X * position.Y) - (position.X * previousPosition.Y);
+                }
+
+                previousPosition = position;
+            }
+            area += (previousPosition.X * firstPosition.Y) - (firstPosition.X * previousPosition.Y);
+
+            return area * 0.5;
+        }
+
+        public static PolygonWindingOrder ComputeWindingOrder(IEnumerable<Vector2D> positions)
+        {
+            return (ComputeArea(positions) >= 0.0) ? PolygonWindingOrder.Counterclockwise : PolygonWindingOrder.Clockwise;
+        }
+    }
+}
---
+++ Source/Tests/Core/Polygons/SimplePolygonAlgorithms.cs
@@ -0,0 +1,84 @@
+﻿#region License
+//
+// (C) Copyright 2010 Patrick Cozzi, Deron Ohlarik
+//
+// Distributed under the Boost Software License, Version 1.0.
+// See License.txt or http://www.boost.org/LICENSE_1_0.txt.
+//
+#endregion
+
+using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace OpenGlobe.Core
+{
+    [TestFixture]
+    public class SimplePolygonAlgorithmsTests
+    {
+        [Test]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void WindingOrderNull()
+        {
+            SimplePolygonAlgorithms.ComputeWindingOrder(null);
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void WindingOrderEmpty()
+        {
+            SimplePolygonAlgorithms.ComputeWindingOrder(new List<Vector2D>());
+        }
+
+        
+        [Test]
+        public void WindingOrderCounterclockwiseTriangle()
+        {
+            IList<Vector2D> positions = new List<Vector2D>();
+            positions.Add(new Vector2D(0, 0));
+            positions.Add(new Vector2D(1, 0)); 
+            positions.Add(new Vector2D(1, 1));
+
+            Assert.AreEqual(PolygonWindingOrder.Counterclockwise, SimplePolygonAlgorithms.ComputeWindingOrder(positions));
+            Assert.AreEqual(0.5, SimplePolygonAlgorithms.ComputeArea(positions));
+        }
+
+        [Test]
+        public void WindingOrderClockwiseTriangle()
+        {
+            IList<Vector2D> positions = new List<Vector2D>();
+            positions.Add(new Vector2D(0, 0));
+            positions.Add(new Vector2D(1, 1));
+            positions.Add(new Vector2D(1, 0));
+
+            Assert.AreEqual(PolygonWindingOrder.Clockwise, SimplePolygonAlgorithms.ComputeWindingOrder(positions));
+            Assert.AreEqual(-0.5, SimplePolygonAlgorithms.ComputeArea(positions));
+        }
+
+        [Test]
+        public void WindingOrderCounterclockwiseDiamond()
+        {
+            IList<Vector2D> positions = new List<Vector2D>();
+            positions.Add(new Vector2D(0, -0.5));
+            positions.Add(new Vector2D(0.5, 0));
+            positions.Add(new Vector2D(0, 0.5));
+            positions.Add(new Vector2D(-0.5, 0));
+
+            Assert.AreEqual(PolygonWindingOrder.Counterclockwise, SimplePolygonAlgorithms.ComputeWindingOrder(positions));
+            Assert.AreEqual(0.5, SimplePolygonAlgorithms.ComputeArea(positions));
+        }
+
+        [Test]
+        public void WindingOrderClockwiseDiamond()
+        {
+            IList<Vector2D> positions = new List<Vector2D>();
+            positions.Add(new Vector2D(0, -0.5));
+            positions.Add(new Vector2D(-0.5, 0));
+            positions.Add(new Vector2D(0, 0.5));
+            positions.Add(new Vector2D(0.5, 0));
+
+            Assert.AreEqual(PolygonWindingOrder.Clockwise, SimplePolygonAlgorithms.ComputeWindingOrder(positions));
+            Assert.AreEqual(-0.5, SimplePolygonAlgorithms.ComputeArea(positions));
+        }
+    }
+}
