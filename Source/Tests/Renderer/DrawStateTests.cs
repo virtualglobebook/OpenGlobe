99166c30e367eaa01e3a914bea45f0a0bb0e582b
pjcozzi <pjcozzi@6eae0abb-6483-4171-a68c-a7d7e9ebe9e5>
2010-10-06 20:33:37 +0000

    Example comparison function for DrawState.

---
+++ Source/Tests/Renderer/DrawStateTests.cs
@@ -0,0 +1,60 @@
+﻿#region License
+//
+// (C) Copyright 2010 Patrick Cozzi and Deron Ohlarik
+//
+// Distributed under the Boost Software License, Version 1.0.
+// See License.txt or http://www.boost.org/LICENSE_1_0.txt.
+//
+#endregion
+
+using NUnit.Framework;
+using System.Drawing;
+using System.Collections.Generic;
+using System;
+
+namespace OpenGlobe.Renderer
+{
+    [TestFixture]
+    public class DrawStateTests
+    {
+        private static int CompareDrawStates(DrawState left, DrawState right)
+        {
+            // Sort by shader first
+            int leftShader = left.ShaderProgram.GetHashCode();
+            int rightShader = right.ShaderProgram.GetHashCode();
+
+            if (leftShader < rightShader)
+            {
+                return -1;
+            }
+            else if (leftShader > rightShader)
+            {
+                return 1;
+            }
+
+            // Shaders are equal, compare depth test enabled
+            int leftEnabled = Convert.ToInt32(left.RenderState.DepthTest.Enabled);
+            int rightEnabled = Convert.ToInt32(right.RenderState.DepthTest.Enabled);
+                
+            if (leftEnabled < rightEnabled)
+            {
+                return -1;
+            }
+            else if (rightEnabled > leftEnabled)
+            {
+                return 1;
+            }
+
+            // TODO: continue comparing other states in order of most to least expensive...
+            return 0;
+        }
+
+        [Test]
+        public void Sort()
+        {
+            List<DrawState> drawStates = new List<DrawState>();
+
+            drawStates.Sort(CompareDrawStates);
+        }
+    }
+}
