f2f214708173da5be42bf5d4f2fb0f00823df613
pjcozzi <pjcozzi@6eae0abb-6483-4171-a68c-a7d7e9ebe9e5>
2010-11-11 18:39:49 +0000

    Added skeleton for 4x4 matrix.

---
+++ Source/Core/Matrices/Matrix4D.cs
@@ -0,0 +1,168 @@
+﻿#region License
+//
+// (C) Copyright 2010 Patrick Cozzi and Kevin Ring
+//
+// Distributed under the Boost Software License, Version 1.0.
+// See License.txt or http://www.boost.org/LICENSE_1_0.txt.
+//
+#endregion
+
+using System;
+using System.Globalization;
+using System.Runtime.InteropServices;
+
+namespace OpenGlobe.Core
+{
+    /// <summary>
+    /// 4x4 matrix - 4 columns and 4 rows.
+    /// </summary>
+    public class Matrix4D
+    {
+        public Matrix4D()
+        {
+            _values = new double[NumberOfComponents];
+        }
+
+        public Matrix4D(double value)
+        {
+            _values = new double[] 
+            { 
+                value, value, value, value,
+                value, value, value, value,
+                value, value, value, value,
+                value, value, value, value
+            };
+        }
+
+        public Matrix4D(
+            double column0row0, double column1row0, double column2row0, double column3row0,
+            double column0row1, double column1row1, double column2row1, double column3row1,
+            double column0row2, double column1row2, double column2row2, double column3row2,
+            double column0row3, double column1row3, double column2row3, double column3row3)
+        {
+            _values = new double[] 
+            { 
+                column0row0, column0row1, column0row2, column0row3,
+                column1row0, column1row1, column1row2, column1row3,
+                column2row0, column2row1, column2row2, column2row3,
+                column3row0, column3row1, column3row2, column3row3
+            };
+        }
+
+        public int NumberOfComponents
+        {
+            get { return 16; }
+        }
+
+        public double Column0Row0 { get { return _values[0]; } }
+        public double Column0Row1 { get { return _values[1]; } }
+        public double Column0Row2 { get { return _values[2]; } }
+        public double Column0Row3 { get { return _values[3]; } }
+
+        public double Column1Row0 { get { return _values[4]; } }
+        public double Column1Row1 { get { return _values[5]; } }
+        public double Column1Row2 { get { return _values[6]; } }
+        public double Column1Row3 { get { return _values[7]; } }
+
+        public double Column2Row0 { get { return _values[8]; } }
+        public double Column2Row1 { get { return _values[9]; } }
+        public double Column2Row2 { get { return _values[10]; } }
+        public double Column2Row3 { get { return _values[11]; } }
+
+        public double Column3Row0 { get { return _values[12]; } }
+        public double Column3Row1 { get { return _values[13]; } }
+        public double Column3Row2 { get { return _values[14]; } }
+        public double Column3Row3 { get { return _values[15]; } }
+
+/*
+        public static Matrix4F DoubleToFloat(Matrix4D<double> value)
+        {
+            return new Matrix4F(
+                (float)value.Column0Row0,
+                (float)value.Column1Row0,
+                (float)value.Column2Row0,
+                (float)value.Column3Row0,
+                (float)value.Column0Row1,
+                (float)value.Column1Row1,
+                (float)value.Column2Row1,
+                (float)value.Column3Row1,
+                (float)value.Column0Row2,
+                (float)value.Column1Row2,
+                (float)value.Column2Row2,
+                (float)value.Column3Row2,
+                (float)value.Column0Row3,
+                (float)value.Column1Row3,
+                (float)value.Column2Row3,
+                (float)value.Column3Row3);
+        }
+*/
+
+        public bool Equals(Matrix4D other)
+        {
+            //TODO
+            //if (other == null)
+            //{
+            //    return false;
+            //}
+
+            for (int i = 0; i < _values.Length; ++i)
+            {
+                if (!_values[i].Equals(other._values[i]))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public static bool operator ==(Matrix4D left, Matrix4D right)
+        {
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(Matrix4D left, Matrix4D right)
+        {
+            return !left.Equals(right);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is Matrix4D)
+            {
+                return Equals((Matrix4D)obj);
+            }
+            return false;
+        }
+
+        public override string ToString()
+        {
+            return string.Format(CultureInfo.CurrentCulture,
+                "Rows: \n({0:n}, {1:n}, {2:n}, {3:n}) \n({4:n}, {5:n}, {6:n}, {7:n}) \n({8:n}, {9:n}, {10:n}, {11:n}) \n({12:n}, {13:n}, {14:n}, {15:n})",
+                Column0Row0, Column1Row0, Column2Row0, Column3Row0,
+                Column0Row1, Column1Row1, Column2Row1, Column3Row1,
+                Column0Row2, Column1Row2, Column2Row2, Column3Row2,
+                Column0Row3, Column1Row3, Column2Row3, Column3Row3);
+        }
+
+        public override int GetHashCode()
+        {
+            int hashCode = Convert.ToInt32(_values[0]);
+
+            for (int i = 1; i < _values.Length; ++i)
+            {
+                hashCode = hashCode ^ Convert.ToInt32(_values[i]);
+
+            }
+
+            return hashCode;
+        }
+
+        /// <summary>
+        /// Shame on you if you change elements in this array.  This type is supposed to be immutable.
+        /// </summary>
+        public double[] ReadOnlyColumnMajorValues { get { return _values; } }
+
+        private readonly double[] _values;       // Column major
+    }
+}
---
+++ Source/Tests/Core/Matrices/Matrix4DTests.cs
@@ -0,0 +1,177 @@
+﻿#region License
+//
+// (C) Copyright 2010 Patrick Cozzi and Kevin Ring
+//
+// Distributed under the Boost Software License, Version 1.0.
+// See License.txt or http://www.boost.org/LICENSE_1_0.txt.
+//
+#endregion
+
+using NUnit.Framework;
+
+namespace OpenGlobe.Core
+{
+    [TestFixture]
+    public class Matrix4DTests
+    {
+        [Test]
+        public void Construct0()
+        {
+            Matrix4D m = new Matrix4D();
+
+            for (int i = 0; i < m.NumberOfComponents; ++i)
+            {
+                Assert.AreEqual(0.0, m.ReadOnlyColumnMajorValues[0], 1e-14);
+            }
+        }
+
+        [Test]
+        public void Construct1()
+        {
+            Matrix4D m = new Matrix4D(1.0);
+
+            for (int i = 0; i < m.NumberOfComponents; ++i)
+            {
+                Assert.AreEqual(1.0, m.ReadOnlyColumnMajorValues[0], 1e-14);
+            }
+        }
+
+        [Test]
+        public void Construct2()
+        {
+            Matrix4D m = new Matrix4D(
+                1.0, 2.0, 3.0, 4.0,
+                5.0, 6.0, 7.0, 8.0,
+                9.0, 10.0, 11.0, 12.0,
+                13.0, 14.0, 15.0, 16.0);
+
+            Assert.AreEqual(1.0, m.Column0Row0);
+            Assert.AreEqual(2.0, m.Column1Row0);
+            Assert.AreEqual(3.0, m.Column2Row0);
+            Assert.AreEqual(4.0, m.Column3Row0);
+            Assert.AreEqual(5.0, m.Column0Row1);
+            Assert.AreEqual(6.0, m.Column1Row1);
+            Assert.AreEqual(7.0, m.Column2Row1);
+            Assert.AreEqual(8.0, m.Column3Row1);
+            Assert.AreEqual(9.0, m.Column0Row2);
+            Assert.AreEqual(10.0, m.Column1Row2);
+            Assert.AreEqual(11.0, m.Column2Row2);
+            Assert.AreEqual(12.0, m.Column3Row2);
+            Assert.AreEqual(13.0, m.Column0Row3);
+            Assert.AreEqual(14.0, m.Column1Row3);
+            Assert.AreEqual(15.0, m.Column2Row3);
+            Assert.AreEqual(16.0, m.Column3Row3);
+
+            Assert.AreEqual(1.0, m.ReadOnlyColumnMajorValues[0]);
+            Assert.AreEqual(5.0, m.ReadOnlyColumnMajorValues[1]);
+            Assert.AreEqual(9.0, m.ReadOnlyColumnMajorValues[2]);
+            Assert.AreEqual(13.0, m.ReadOnlyColumnMajorValues[3]);
+            Assert.AreEqual(2.0, m.ReadOnlyColumnMajorValues[4]);
+            Assert.AreEqual(6.0, m.ReadOnlyColumnMajorValues[5]);
+            Assert.AreEqual(10.0, m.ReadOnlyColumnMajorValues[6]);
+            Assert.AreEqual(14.0, m.ReadOnlyColumnMajorValues[7]);
+            Assert.AreEqual(3.0, m.ReadOnlyColumnMajorValues[8]);
+            Assert.AreEqual(7.0, m.ReadOnlyColumnMajorValues[9]);
+            Assert.AreEqual(11.0, m.ReadOnlyColumnMajorValues[10]);
+            Assert.AreEqual(15.0, m.ReadOnlyColumnMajorValues[11]);
+            Assert.AreEqual(4.0, m.ReadOnlyColumnMajorValues[12]);
+            Assert.AreEqual(8.0, m.ReadOnlyColumnMajorValues[13]);
+            Assert.AreEqual(12.0, m.ReadOnlyColumnMajorValues[14]);
+            Assert.AreEqual(16.0, m.ReadOnlyColumnMajorValues[15]);
+        }
+
+/*
+        [Test]
+        public void DoubleToFloat()
+        {
+            Matrix4D m = new Matrix4D(
+                1.0, 2.0, 3.0, 4.0,
+                5.0, 6.0, 7.0, 8.0,
+                9.0, 10.0, 11.0, 12.0,
+                13.0, 14.0, 15.0, 16.0);
+
+            Matrix4F mf = Matrix4D.DoubleToFloat(m);
+
+            Assert.AreEqual(1.0f, mf.Column0Row0, 1e-7);
+            Assert.AreEqual(2.0f, mf.Column1Row0, 1e-7);
+            Assert.AreEqual(3.0f, mf.Column2Row0, 1e-7);
+            Assert.AreEqual(4.0f, mf.Column3Row0, 1e-7);
+            Assert.AreEqual(5.0f, mf.Column0Row1, 1e-7);
+            Assert.AreEqual(6.0f, mf.Column1Row1, 1e-7);
+            Assert.AreEqual(7.0f, mf.Column2Row1, 1e-7);
+            Assert.AreEqual(8.0f, mf.Column3Row1, 1e-7);
+            Assert.AreEqual(9.0f, mf.Column0Row2, 1e-7);
+            Assert.AreEqual(10.0f, mf.Column1Row2, 1e-7);
+            Assert.AreEqual(11.0f, mf.Column2Row2, 1e-7);
+            Assert.AreEqual(12.0f, mf.Column3Row2, 1e-7);
+            Assert.AreEqual(13.0f, mf.Column0Row3, 1e-7);
+            Assert.AreEqual(14.0f, mf.Column1Row3, 1e-7);
+            Assert.AreEqual(15.0f, mf.Column2Row3, 1e-7);
+            Assert.AreEqual(16.0f, mf.Column3Row3, 1e-7);
+        }
+*/
+
+        [Test]
+        public void Equals()
+        {
+            Matrix4D a = new Matrix4D(
+                1.0, 2.0, 3.0, 4.0,
+                5.0, 6.0, 7.0, 8.0,
+                9.0, 10.0, 11.0, 12.0,
+                13.0, 14.0, 15.0, 16.0);
+            Matrix4D b = new Matrix4D(0.0);
+            Matrix4D c = new Matrix4D(
+                1.0, 2.0, 3.0, 4.0,
+                5.0, 6.0, 7.0, 8.0,
+                9.0, 10.0, 11.0, 12.0,
+                13.0, 14.0, 15.0, 16.0);
+
+            Assert.IsTrue(a.Equals(c));
+            Assert.IsTrue(c.Equals(a));
+            Assert.IsTrue(a == c);
+            Assert.IsTrue(c == a);
+            Assert.IsFalse(c != a);
+            Assert.IsFalse(c != a);
+            Assert.IsFalse(a.Equals(b));
+            Assert.IsFalse(b.Equals(a));
+            Assert.IsFalse(a == b);
+            Assert.IsFalse(b == a);
+            Assert.IsTrue(a != b);
+            Assert.IsTrue(b != a);
+
+            object objA = a;
+            object objB = b;
+            object objC = c;
+
+            Assert.IsTrue(a.Equals(objA));
+            Assert.IsTrue(a.Equals(objC));
+            Assert.IsFalse(a.Equals(objB));
+
+            Assert.IsTrue(objA.Equals(objC));
+            Assert.IsFalse(objA.Equals(objB));
+
+            //TODO
+            //Assert.IsFalse(a.Equals(null));
+            Assert.IsFalse(a.Equals(5));
+        }
+
+        [Test]
+        public void TestGetHashCode()
+        {
+            Matrix4D a = new Matrix4D(
+                1.0, 2.0, 3.0, 4.0,
+                5.0, 6.0, 7.0, 8.0,
+                9.0, 10.0, 11.0, 12.0,
+                13.0, 14.0, 15.0, 16.0);
+            Matrix4D b = new Matrix4D(0.0);
+            Matrix4D c = new Matrix4D(
+                1.0, 2.0, 3.0, 4.0,
+                5.0, 6.0, 7.0, 8.0,
+                9.0, 10.0, 11.0, 12.0,
+                13.0, 14.0, 15.0, 16.0);
+
+            Assert.AreEqual(a.GetHashCode(), c.GetHashCode());
+            Assert.AreNotEqual(a.GetHashCode(), b.GetHashCode());
+        }
+    }
+}
