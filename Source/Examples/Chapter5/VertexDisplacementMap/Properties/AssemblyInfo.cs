cbc78d9b4f9ad0b55239a9bc1cd64277b78386da
kring789 <kring789@6eae0abb-6483-4171-a68c-a7d7e9ebe9e5>
2010-11-14 18:08:11 +0000

    Undo accidental change to the VertexDisplacementMap AssemblyInfo.cs file.  SVN is weird.

--- Source/Examples/Chapter5/VertexDisplacementMap/Properties/AssemblyInfo.cs
+++ Source/Examples/Chapter5/VertexDisplacementMap/Properties/AssemblyInfo.cs
@@ -1,6 +1,6 @@
 ﻿#region License
 //
-// (C) Copyright 2010 Patrick Cozzi and Kevin Ring
+// (C) Copyright 2010 Patrick Cozzi and Deron Ohlarik
 //
 // Distributed under the Boost Software License, Version 1.0.
 // See License.txt or http://www.boost.org/LICENSE_1_0.txt.
@@ -12,11 +12,11 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("OpenGlobe.Examples.ClipmapTerrain")]
+[assembly: AssemblyTitle("OpenGlobe.Examples.VertexDisplacementMap")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("OpenGlobe.Examples.ClipmapTerrain")]
+[assembly: AssemblyProduct("OpenGlobe.Examples.VertexDisplacementMap")]
 [assembly: AssemblyCopyright("Copyright ©  2010")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -24,7 +24,7 @@ using System.Runtime.InteropServices;
 [assembly: CLSCompliant(true)]
 [assembly: ComVisible(false)]
 
-[assembly: Guid("A5DAC768-EFBB-4D1A-9AD6-3F2281488A8D")]
+[assembly: Guid("74832ef4-21d0-47b6-8d1a-e21de4ecdc7f")]
 
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
