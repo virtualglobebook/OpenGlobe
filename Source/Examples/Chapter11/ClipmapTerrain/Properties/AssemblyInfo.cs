e96399f40716a1e286e7d2327b82a008359dab82
kring789 <kring789@6eae0abb-6483-4171-a68c-a7d7e9ebe9e5>
2010-11-14 18:10:05 +0000

    Add the missing AssemblyInfo.cs for real this time.

---
+++ Source/Examples/Chapter11/ClipmapTerrain/Properties/AssemblyInfo.cs
@@ -0,0 +1,30 @@
+﻿#region License
+//
+// (C) Copyright 2010 Patrick Cozzi and Kevin Ring
+//
+// Distributed under the Boost Software License, Version 1.0.
+// See License.txt or http://www.boost.org/LICENSE_1_0.txt.
+//
+#endregion
+
+using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("OpenGlobe.Examples.ClipmapTerrain")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("OpenGlobe.Examples.ClipmapTerrain")]
+[assembly: AssemblyCopyright("Copyright ©  2010")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: CLSCompliant(true)]
+[assembly: ComVisible(false)]
+
+[assembly: Guid("A5DAC768-EFBB-4D1A-9AD6-3F2281488A8D")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
