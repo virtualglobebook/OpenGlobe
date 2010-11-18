5fe29f8d9ea333b8278c3357e9268d41918a6d14
kring789 <kring789@6eae0abb-6483-4171-a68c-a7d7e9ebe9e5>
2010-11-18 20:07:07 +0000

    Chunked LOD.  Not much progress so far.

---
+++ Source/Scene/Terrain/ChunkedTerrain.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OpenGlobe.Scene.Terrain
+{
+    public class ChunkedTerrain
+    {
+        public ChunkedTerrain(string chuFilename, string tqtFilename) :
+            this(new ChunkedTerrainTree(chuFilename))
+        {
+
+        }
+
+        public ChunkedTerrain(ChunkedTerrainTree tree)
+        {
+            _tree = tree;
+        }
+
+        private ChunkedTerrainTree _tree;
+    }
+}
---
+++ Source/Scene/Terrain/ChunkedTerrainTree.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OpenGlobe.Scene.Terrain
+{
+    public class ChunkedTerrainTree
+    {
+        public ChunkedTerrainTree(string chuFilename)
+        {
+        }
+    }
+}
