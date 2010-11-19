445f3ac98d6e2caad4e671966c063b2ab4bf4a9d
kring789 <kring789@6eae0abb-6483-4171-a68c-a7d7e9ebe9e5>
2010-11-19 18:57:45 +0000

    Working toward out-of-core rendering for clipmaps.  This is just the beginning of explicit tiles for raster terrains.

---
+++ Source/Scene/Terrain/RasterTerrainTile.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OpenGlobe.Scene.Terrain
+{
+    public abstract class RasterTerrainTile
+    {
+        public RasterTerrainTile(RasterTerrainLevel level, RasterTerrainTileIdentifier identifier)
+        {
+            _level = level;
+            _identifier = identifier;
+        }
+
+        public RasterTerrainLevel Level
+        {
+            get { return _level; }
+        }
+
+        public RasterTerrainTileIdentifier Identifier
+        {
+            get { return _identifier; }
+        }
+
+        public abstract RasterTerrainTileStatus Status { get; }
+
+        public abstract RasterTerrainTile SouthwestChild { get; }
+        public abstract RasterTerrainTile SoutheastChild { get; }
+        public abstract RasterTerrainTile NorthwestChild { get; }
+        public abstract RasterTerrainTile NortheastChild { get; }
+
+        /// <summary>
+        /// Loads the tile into system memory, reading it from disk or a network server as necessary.
+        /// This method does not return until the tile is loaded.
+        /// </summary>
+        public abstract void Load();
+
+        /// <summary>
+        /// Gets a subset of posts from the tile and copies them into destination array.  This method should
+        /// only be called when <see cref="Status"/> is <see cref="RasterTerrainTileStatus.Loaded"/>.
+        /// </summary>
+        /// <param name="west">The westmost post to copy.</param>
+        /// <param name="south">The southernmost post to copy.</param>
+        /// <param name="east">The easternmost post to copy.</param>
+        /// <param name="north">The northernmost post to copy.</param>
+        /// <param name="destination">The destination array to receive the post data.</param>
+        /// <param name="stride">The number of posts in a row of the destination array.</param>
+        /// <exception cref="InvalidOperationException">
+        /// The tile is not loaded.
+        /// </exception>
+        public abstract void GetPosts(int west, int south, int east, int north, float[] destination, int stride);
+
+        private RasterTerrainTileIdentifier _identifier;
+        private RasterTerrainLevel _level;
+    }
+}
---
+++ Source/Scene/Terrain/RasterTerrainTileIdentifier.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OpenGlobe.Scene.Terrain
+{
+    public struct RasterTerrainTileIdentifier : IEquatable<RasterTerrainTileIdentifier>
+    {
+        public RasterTerrainTileIdentifier(int x, int y)
+        {
+            _x = x;
+            _y = y;
+        }
+
+        public int X
+        {
+            get { return _x; }
+        }
+
+        public int Y
+        {
+            get { return _y; }
+        }
+
+        public bool Equals(RasterTerrainTileIdentifier other)
+        {
+            return _x == other._x && _y == other._y;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is RasterTerrainTileIdentifier))
+                return false;
+            return Equals((RasterTerrainTileIdentifier)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return _x.GetHashCode() ^ _y.GetHashCode();
+        }
+
+        private int _x;
+        private int _y;
+    }
+}
