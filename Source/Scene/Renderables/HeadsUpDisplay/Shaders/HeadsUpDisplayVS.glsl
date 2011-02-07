386eb5ca9b9775a912da2303b3c7d3c317787189
pjcozzi <pjcozzi@6eae0abb-6483-4171-a68c-a7d7e9ebe9e5>
2011-02-07 18:01:40 +0000

    Removed geometry shader from heads up display.

--- Source/Scene/Renderables/HeadsUpDisplay/Shaders/HeadsUpDisplayVS.glsl
+++ Source/Scene/Renderables/HeadsUpDisplay/Shaders/HeadsUpDisplayVS.glsl
@@ -6,9 +6,19 @@
 // See License.txt or http://www.opensource.org/licenses/mit-license.php.
 //
 
-layout(location = og_positionVertexLocation) in vec4 position;
+layout(location = og_positionVertexLocation) in vec2 position;
+layout(location = og_textureCoordinateVertexLocation) in vec2 textureCoordinates;
+
+out vec2 fsTextureCoordinates;
+
+uniform vec2 u_originScale;
+uniform vec2 u_halfTextureSize;
 
 void main()                     
 {
-    gl_Position = position;
+    vec2 center = position + (u_originScale * u_halfTextureSize);
+    vec2 halfSize = u_halfTextureSize * ((textureCoordinates * 2.0) - 1.0);
+
+    gl_Position = og_viewportOrthographicMatrix * vec4(center + halfSize, 0.0, 1.0);
+    fsTextureCoordinates = textureCoordinates;
 }
